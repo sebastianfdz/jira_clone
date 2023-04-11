4d95495243f79f940b863ce694af755d6f317cea
sebastianfdz <fdz.seb@gmail.com>
2023-04-10 21:19:20 -0600

    Add backlog issue with draggable wrapper. Not working yet

---
+++ src/components/Backlog/Issue.tsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import { Draggable } from "react-beautiful-dnd";
+
+export const Issue: React.FC<{
+  id: string;
+  index: number;
+}> = ({ id, index }) => {
+  return (
+    <Fragment>
+      <Draggable draggableId={id} index={index}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <h3>Issue</h3>
+            {snapshot.isDragging ? "Dragging" : "Not dragging"}
+          </div>
+        )}
+      </Draggable>
+    </Fragment>
+  );
+};
