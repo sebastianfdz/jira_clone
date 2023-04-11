9c11a3ac4282b3cc1e6982e25c9029d3e36a0152
sebastianfdz <fdz.seb@gmail.com>
2023-04-10 21:17:27 -0600

    Remove boilerplate design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,13 +12,7 @@ const Home: NextPage = () => {
           href="https://www.vectorlogo.zone/logos/atlassian_jira/atlassian_jira-icon.svg"
         />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-        </div>
-      </main>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]"></main>
     </>
   );
 };
