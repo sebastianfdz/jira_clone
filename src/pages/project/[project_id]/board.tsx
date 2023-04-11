614365e717c0b36d96d4aa3898eaca5150871892
sebastianfdz <fdz.seb@gmail.com>
2023-04-10 21:19:38 -0600

    Add nested routes for project

---
+++ src/pages/project/[project_id]/board.tsx
@@ -0,0 +1,8 @@
+import { type NextPage } from "next";
+import { Fragment } from "react";
+
+const Board: NextPage = () => {
+  return <Fragment>Board</Fragment>;
+};
+
+export default Board;
---
+++ src/pages/project/[project_id]/index.tsx
@@ -0,0 +1,8 @@
+import { type NextPage } from "next";
+import { Fragment } from "react";
+
+const Project: NextPage = () => {
+  return <Fragment>Project</Fragment>;
+};
+
+export default Project;
