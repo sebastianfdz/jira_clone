5be1b49ee13e1fc4134af14a143cf81f55c0f3d7
sebastianfdz <fdz.seb@gmail.com>
2023-04-12 12:00:50 -0600

    Add app and components directory paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
