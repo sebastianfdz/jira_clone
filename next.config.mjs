129ca3ea18805e466f28989420b32e563baa2924
sebastianfdz <fdz.seb@gmail.com>
2023-04-13 19:53:01 -0600

    Add unsplash domain to images

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@
 const config = {
   reactStrictMode: true,
   experimental: { appDir: true },
+
+  images: {
+    domains: ["images.clerk.dev", "images.unsplash.com"],
+  },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
