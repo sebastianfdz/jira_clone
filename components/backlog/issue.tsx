682bc596b3ab3d0623ef7d8d53d4a9272a1cadfe
sebastianfdz <fdz.seb@gmail.com>
2023-04-13 19:54:27 -0600

    Add avatar and children icon to issue

--- components/backlog/issue.tsx
+++ components/backlog/issue.tsx
@@ -3,6 +3,10 @@ import { Draggable } from "react-beautiful-dnd";
 import { IssueIcon } from "../issue-icon";
 import { Button } from "../ui/button";
 import { MdEdit } from "react-icons/md";
+import clsx from "clsx";
+import { BsThreeDots } from "react-icons/bs";
+import { Avatar } from "@/components/avatar";
+import { ChildrenTreeIcon } from "../icons";
 
 export const Issue: React.FC<{
   id: string;
@@ -10,13 +14,16 @@ export const Issue: React.FC<{
 }> = ({ id, index }) => {
   return (
     <Fragment>
-      <Draggable draggableId={id + id} index={index}>
-        {({ innerRef, dragHandleProps, draggableProps }) => (
+      <Draggable draggableId={id} index={index}>
+        {({ innerRef, dragHandleProps, draggableProps }, { isDragging }) => (
           <div
             ref={innerRef}
             {...draggableProps}
             {...dragHandleProps}
-            className="group flex items-center justify-between border border-gray-300 bg-white p-3 hover:bg-gray-50"
+            className={clsx(
+              isDragging ? "bg-blue-100" : "bg-white",
+              "group flex items-center justify-between border border-gray-300  p-3 hover:bg-gray-50"
+            )}
           >
             <div className="flex items-center justify-between gap-x-2">
               <IssueIcon issueType="task" />
@@ -24,20 +31,24 @@ export const Issue: React.FC<{
               <IssueIcon issueType="bug" />
               <div className="text-gray-600">SP2023-128</div>
               <div>Add environment variables in production</div>
-
-              <Button className="invisible w-0 bg-transparent px-0 hover:bg-gray-200 group-hover:visible group-hover:w-fit group-hover:px-1.5">
+              <Button className="invisible w-0 px-0 group-hover:visible group-hover:w-fit group-hover:bg-transparent group-hover:px-1.5 group-hover:hover:bg-gray-200">
                 <MdEdit className="text-sm" />
               </Button>
-
-              <div className="spa rounded-md bg-indigo-500 bg-opacity-30 px-2 text-sm font-bold tracking-wide text-indigo-700">
+              <div className="spa rounded-[3px] bg-indigo-500 bg-opacity-30 px-2 text-sm font-bold tracking-wide text-indigo-700">
                 EPIC-LABEL
               </div>
             </div>
             <div className="flex items-center justify-between">
-              <div>[children_icon]</div>
+              <ChildrenTreeIcon className="mx-2 text-gray-600" />
               <div>[status_dd_button]</div>
-              <div>[assignee_avatar]</div>
-              <div>[ellipsis_hover]</div>
+              <Avatar
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+                className="mx-1"
+              />
+              <Button className="invisible ml-2 group-hover:visible ">
+                <BsThreeDots className="sm:text-xl" />
+              </Button>
             </div>
           </div>
         )}
