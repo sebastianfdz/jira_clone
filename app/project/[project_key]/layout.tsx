cfd4f876b5232ea29f56e66c1be0435cab37784d
sebastianfdz <fdz.seb@gmail.com>
2023-04-15 10:35:43 -0600

    Adjust styling in main for layout

--- app/project/[project_key]/layout.tsx
+++ app/project/[project_key]/layout.tsx
@@ -4,7 +4,7 @@ const ProjectLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex h-screen w-72 items-center justify-center border bg-blue-100">
         Sidebar
       </div>
-      <main>{children}</main>
+      <main className="w-full">{children}</main>
     </div>
   );
 };
