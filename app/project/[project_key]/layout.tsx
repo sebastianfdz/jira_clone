e17d4e069726608a0994db03e6935a21f6ef0c15
sebastianfdz <fdz.seb@gmail.com>
2023-04-15 10:14:16 -0600

    Setup emtpy layout for project.

---
+++ app/project/[project_key]/layout.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
