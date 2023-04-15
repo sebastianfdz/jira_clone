5e5841a306758af97cd1ae57bbaef5833de16aa9
sebastianfdz <fdz.seb@gmail.com>
2023-04-14 19:57:41 -0600

    Add issue status counter component

---
+++ components/backlog/issue-status-count.tsx
@@ -0,0 +1,27 @@
+import { type IsseCountType } from "@/utils/types";
+import clsx from "clsx";
+import { Fragment } from "react";
+
+const IssueStatusCount: React.FC<{ statusCount: IsseCountType }> = ({
+  statusCount,
+}) => {
+  return (
+    <Fragment>
+      {Object.entries(statusCount ?? {})?.map(([status, count]) => (
+        <span
+          key={status}
+          className={clsx(
+            status == "TODO" && "bg-zinc-300 text-black",
+            status == "IN_PROGRESS" && "bg-blue-700 text-white",
+            status == "DONE" && "bg-green-700 text-white",
+            "flex h-5 items-center justify-center rounded-full px-1.5 py-0.5 text-sm font-semibold"
+          )}
+        >
+          {count}
+        </span>
+      ))}
+    </Fragment>
+  );
+};
+
+export { IssueStatusCount };
