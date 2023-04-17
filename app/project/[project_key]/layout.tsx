d21fb44a8ad07610a088d618a88cf1beaf28c93e
sebastianfdz <fdz.seb@gmail.com>
2023-04-17 10:35:37 -0600

    Add top navbar placeholder

--- app/project/[project_key]/layout.tsx
+++ app/project/[project_key]/layout.tsx
@@ -2,9 +2,14 @@ import { Sidebar } from "@/components/sidebar";
 
 const ProjectLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="w-full">{children}</main>
+    <div>
+      <div className="flex h-12 w-full items-center justify-center border-b">
+        top nav bar
+      </div>
+      <div className="flex">
+        <Sidebar />
+        <main className="w-full">{children}</main>
+      </div>
     </div>
   );
 };
