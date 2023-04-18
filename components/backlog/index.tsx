52c23c6994250576655fb411fc0a1aff24739296
sebastianfdz <fdz.seb@gmail.com>
2023-04-18 12:11:29 -0600

    Use imported issue details

--- components/backlog/index.tsx
+++ components/backlog/index.tsx
@@ -1,82 +1,50 @@
 "use client";
-import { Fragment } from "react";
-import {
-  DragDropContext,
-  type DraggableLocation,
-  type DropResult,
-} from "react-beautiful-dnd";
-import { Container } from "@/components/ui/container";
-import { BacklogList } from "./backlog-list";
-import { SprintList } from "./sprint-list";
-import { useBrowser } from "@/hooks/useBrowser";
-// import { moveItemWithinArray } from "@/utils/helpers";
-// import { type RouterOutputs } from "@/utils/api";
+import React, { Fragment, type ReactNode } from "react";
+import { useSearchParams } from "next/navigation";
 import { type Project } from "@prisma/client";
+import Split from "react-split";
+import "./index.css";
+import { ListGroup } from "./list-group";
+import { IssueDetails } from "./issue-details";
 
-export const Backlog: React.FC<{
+const Backlog: React.FC<{
   project: Project;
 }> = ({ project }) => {
-  const sprints = [{ id: "1" }, { id: "2" }, { id: "3" }];
-  const { isBrowser } = useBrowser();
-  console.log("project", project);
+  console.log("project => ", project);
 
-  const onDragEnd = (result: DropResult) => {
-    const {
-      destination,
-      source,
-      //  draggableId
-    } = result;
-    if (!positionHasChanged(source, destination)) return;
-    if (source.droppableId === destination?.droppableId) {
-      // const column = moveItemWithinArray();
-    }
-  };
+  const searchParams = useSearchParams();
+  const selectedIssue = searchParams.get("selectedIssue");
 
   return (
-    <Container>
-      <div className="text-sm text-zinc-500">Breadcrumbs / backlog</div>
-      <h1>Backlog</h1>
-      <div className="h-10 border">Backlog Menu</div>
-      <DragDropContext onDragEnd={onDragEnd}>
-        {isBrowser ? (
-          <Fragment>
-            {sprints.map((sprint) => (
-              <div key={sprint.id} className="my-3">
-                <SprintList {...sprint} />
-              </div>
-            ))}
-            <BacklogList id={"backlog"} />
-          </Fragment>
-        ) : null}
-      </DragDropContext>
-    </Container>
+    <Fragment>
+      {selectedIssue ? (
+        <IssueSplitScreen selectedIssue={selectedIssue}>
+          <ListGroup />
+        </IssueSplitScreen>
+      ) : (
+        <ListGroup />
+      )}
+    </Fragment>
   );
 };
 
-function positionHasChanged(
-  source: DraggableLocation,
-  destination: DraggableLocation | null | undefined
-) {
-  // Snippet taken from https://github.com/oldboyxx/jira_clone
-  if (!destination) return false;
-  const isSameList = destination.droppableId === source.droppableId;
-  const isSamePosition = destination.index === source.index;
-  return !isSameList || !isSamePosition;
-}
+const IssueSplitScreen: React.FC<{
+  children: ReactNode;
+  selectedIssue: string;
+}> = ({ children, selectedIssue }) => {
+  return (
+    <Split
+      direction="horizontal"
+      sizes={[80, 20]}
+      gutterSize={2}
+      gutterAlign="center"
+      cursor="col-resize"
+      className="flex h-full"
+    >
+      <div className="w-full pr-4">{children}</div>
+      <IssueDetails issueId={selectedIssue} />
+    </Split>
+  );
+};
 
-// function cloneAndReorder(
-//   list: unknown[],
-//   startIndex: number,
-//   endIndex: number
-// ) {
-//   const listClone = [...list];
-//   const [removed] = listClone.splice(startIndex, 1);
-//   listClone.splice(endIndex, 0, removed);
-//   return listClone;
-// }
-
-// function getSortedListIssues(issues, columnId) {
-//   issues
-//     .filter((issue) => issue.columnId === columnId)
-//     .sort((a, b) => a.listPosition - b.listPosition);
-// }
+export { Backlog };
