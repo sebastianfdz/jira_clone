a45c429462ed61d1ef634bdabb95afabedd10444
sebastianfdz <fdz.seb@gmail.com>
2023-04-18 11:48:49 -0600

    Move header html to header file

---
+++ components/backlog/header.tsx
@@ -0,0 +1,11 @@
+const BacklogHeader = () => {
+  return (
+    <div className="flex h-fit flex-col">
+      <div className="text-sm text-zinc-500">Breadcrumbs / backlog</div>
+      <h1>Backlog</h1>
+      <div className="h-10">Backlog Menu</div>
+    </div>
+  );
+};
+
+export { BacklogHeader };
