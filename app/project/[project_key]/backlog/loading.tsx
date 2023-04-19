814fcf84fcc8a18b5737baec79fefeb2902169ba
sebastianfdz <fdz.seb@gmail.com>
2023-04-19 01:00:46 -0600

    Remove padding from backlog skeleton

--- app/project/[project_key]/backlog/loading.tsx
+++ app/project/[project_key]/backlog/loading.tsx
@@ -5,24 +5,21 @@ import {
   SprintSearchSkeleton,
   TitleSkeleton,
 } from "@/components/skeletons";
-import { Container } from "@/components/ui/container";
 
 const BacklogSkeleton = () => {
   return (
-    <Container>
-      <div role="status" className="flex animate-pulse flex-col gap-y-4">
-        <BreadCrumbSkeleton />
-        <TitleSkeleton />
-        <SprintSearchSkeleton />
-        <SprintHeaderSkeleton />
-        <div className="mt-4 flex flex-col gap-y-6 px-8">
-          {[...Array(10).keys()].map((el, index) => (
-            <IssueSkeleton key={index} size={index % 2 === 0 ? 300 : 400} />
-          ))}
-        </div>
-        <span className="sr-only">Loading...</span>
+    <div role="status" className="flex animate-pulse flex-col gap-y-4">
+      <BreadCrumbSkeleton />
+      <TitleSkeleton />
+      <SprintSearchSkeleton />
+      <SprintHeaderSkeleton />
+      <div className="mt-3 flex flex-col gap-y-4 px-8">
+        {[...Array(10).keys()].map((el, index) => (
+          <IssueSkeleton key={index} size={index % 2 === 0 ? 300 : 400} />
+        ))}
       </div>
-    </Container>
+      <span className="sr-only">Loading...</span>
+    </div>
   );
 };
 
