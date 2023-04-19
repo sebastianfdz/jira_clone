971f85f47fdc82b68682f136ef77b9eb9e330278
sebastianfdz <fdz.seb@gmail.com>
2023-04-19 00:56:02 -0600

    Fetch project from db

--- server/api/routers/projects.ts
+++ server/api/routers/projects.ts
@@ -1,17 +1,16 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { prisma } from "@/server/db";
 
 export const projectsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({}) => {
-    return [
-      {
-        id: "1",
-        name: "Project 1",
-        description: "Project 1 description",
-      },
-    ];
-  }),
+  getAll: publicProcedure
+    .input(z.object({ key: z.string() }))
+    .query(async ({ input }) => {
+      return await prisma.project.findUnique({
+        where: { key: input.key },
+      });
+    }),
   getOne: publicProcedure
     .input(z.object({ key: z.string() }))
     .query(({ input }) => {
