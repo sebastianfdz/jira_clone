3f34c66ea508464f1f99ddf786eb785a7e7ffc7f
sebastianfdz <fdz.seb@gmail.com>
2023-04-19 00:49:45 -0600

    Simplify split screen component

--- components/backlog/index.tsx
+++ components/backlog/index.tsx
@@ -1,48 +1,27 @@
 "use client";
-import React, { Fragment, type ReactNode } from "react";
-import { useSearchParams } from "next/navigation";
+import React from "react";
 import { type Project } from "@prisma/client";
 import Split from "react-split";
-import "./index.css";
 import { ListGroup } from "./list-group";
 import { IssueDetails } from "./issue-details";
+import { useSelectedIssueContext } from "@/hooks/useSelectedIssue";
+import "./index.css";
+import clsx from "clsx";
 
 const Backlog: React.FC<{
   project: Project;
-}> = ({ project }) => {
-  console.log("project => ", project);
+}> = ({}) => {
+  const { issue } = useSelectedIssueContext();
 
-  const searchParams = useSearchParams();
-  const selectedIssue = searchParams.get("selectedIssue");
-
-  return (
-    <Fragment>
-      {selectedIssue ? (
-        <IssueSplitScreen selectedIssue={selectedIssue}>
-          <ListGroup />
-        </IssueSplitScreen>
-      ) : (
-        <ListGroup />
-      )}
-    </Fragment>
-  );
-};
-
-const IssueSplitScreen: React.FC<{
-  children: ReactNode;
-  selectedIssue: string;
-}> = ({ children, selectedIssue }) => {
   return (
     <Split
-      direction="horizontal"
-      sizes={[80, 20]}
-      gutterSize={2}
-      gutterAlign="center"
-      cursor="col-resize"
+      sizes={issue ? [80, 20] : [100, 0]}
+      gutterSize={issue ? 2 : 0}
       className="flex h-full"
+      minSize={issue ? 100 : 0}
     >
-      <div className="w-full pr-4">{children}</div>
-      <IssueDetails issueId={selectedIssue} />
+      <ListGroup className={clsx(issue && "pr-4")} />
+      <IssueDetails selectedIssue={issue} />
     </Split>
   );
 };
