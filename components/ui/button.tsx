4376f69f5d4d42a7a5cc616bb410223d5baa7d92
sebastianfdz <fdz.seb@gmail.com>
2023-04-19 18:45:01 -0600

    Standardize padding

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,7 +16,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <button
         className={clsx(
           !customColors && "bg-zinc-200 hover:bg-zinc-300",
-          "inline-flex items-center gap-x-1.5 rounded-[3px]  px-2.5 py-1.5  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+          "inline-flex items-center gap-x-1.5 rounded-[3px] p-1.5  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
           className
         )}
         ref={ref}
