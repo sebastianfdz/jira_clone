3922fc9207f8b37600ae33b06b3b0d31cd2caaa3
sebastianfdz <fdz.seb@gmail.com>
2023-04-21 07:32:01 -0600

    Add fallback empty user icon

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,21 +1,34 @@
 import clsx from "clsx";
 import Image from "next/image";
+import { Fragment } from "react";
+import { FaUser } from "react-icons/fa";
 type AvatarProps = {
-  src: string;
+  src: string | null;
   alt: string;
   size?: number;
   className?: string;
 };
 const Avatar = ({ src, alt, size = 32, className, ...props }: AvatarProps) => {
   return (
-    <Image
-      className={clsx("rounded-full", className)}
-      src={src}
-      alt={alt}
-      height={size}
-      width={size}
-      {...props}
-    />
+    <Fragment>
+      {src ? (
+        <Image
+          className={clsx("rounded-full", className)}
+          src={src}
+          alt={alt}
+          height={size}
+          width={size}
+          {...props}
+        />
+      ) : (
+        <div
+          className="flex items-center justify-center rounded-full bg-zinc-400 p-1"
+          style={{ width: size, height: size }}
+        >
+          <FaUser className="text-white" />
+        </div>
+      )}
+    </Fragment>
   );
 };
 
