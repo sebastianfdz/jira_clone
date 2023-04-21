17aeabc05b2ae152a7723f31b055de2bddb5e956
sebastianfdz <fdz.seb@gmail.com>
2023-04-21 07:32:26 -0600

    Add optional custom padding prop

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,17 +6,22 @@ type ButtonProps = {
   children: React.ReactNode;
   className?: string;
   customColors?: boolean;
+  customPadding?: boolean;
   href?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, className, customColors, href, ...props }, ref) => {
+  (
+    { children, className, customColors, customPadding, href, ...props },
+    ref
+  ) => {
     if (href) return <Link href={href}>{children}</Link>;
     return (
       <button
         className={clsx(
           !customColors && "bg-zinc-200 hover:bg-zinc-300",
-          "inline-flex items-center gap-x-1.5 rounded-[3px] p-1.5  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+          !customPadding && "p-1.5",
+          "inline-flex items-center rounded-[3px] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
           className
         )}
         ref={ref}
