58535f02d440429c82f3770b6a6add4f2ba725e6
sebastianfdz <fdz.seb@gmail.com>
2023-04-24 08:10:16 -0600

    Use epic selector without children

--- components/backlog/issue-path.tsx
+++ components/backlog/issue-path.tsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
-import { MdEdit } from "react-icons/md";
-import { IssueIcon } from "../issue-icon";
 import { Button } from "../ui/button";
 import { IssueSelectType } from "../issue-select-type";
 import { type IssueType } from "./issue";
 import { IssueSelectEpic } from "../issue-select-epic";
-import { SelectValue } from "../ui/select";
 
 const IssuePath: React.FC<{
   issue: IssueType;
   setIssue: React.Dispatch<React.SetStateAction<string | null>>;
 }> = ({ issue, setIssue }) => {
-  const [showEditButton, setShowEditButton] = useState(false);
-
   return (
     <div className="flex gap-x-3">
       <div
@@ -21,17 +15,8 @@ const IssuePath: React.FC<{
       >
         <IssueSelectEpic
           currentEpic={{ key: "P-SEBB-1", title: "Epic title 1" }}
-        >
-          <SelectValue
-            onMouseEnter={() => setShowEditButton(true)}
-            onMouseLeave={() => setShowEditButton(false)}
-          >
-            {showEditButton ? <MdEdit /> : <IssueIcon issueType="EPIC" />}
-          </SelectValue>
-        </IssueSelectEpic>
+        />
         <Button
-          onMouseEnter={() => setShowEditButton(true)}
-          onMouseLeave={() => setShowEditButton(false)}
           onClick={() => setIssue(issue.epic)}
           customColors
           className=" bg-transparent text-xs text-zinc-500 underline-offset-2 hover:underline"
