242dea8720e46cd310e56da83628b0a345b3b600
sebastianfdz <fdz.seb@gmail.com>
2023-04-29 00:46:16 -0600

    Move empty issue to its own component

--- components/backlog/issue-list.tsx
+++ components/backlog/issue-list.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { Droppable } from "react-beautiful-dnd";
 import { AccordionContent } from "../ui/accordion";
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useState } from "react";
 import { Issue, type IssueType } from "./issue";
 import { Button } from "../ui/button";
 import { AiOutlinePlus } from "react-icons/ai";
-import { IssueSelectType } from "../issue-select-type";
-import clsx from "clsx";
-import { MdCheck, MdClose } from "react-icons/md";
+import { EmtpyIssue } from "./issue-empty";
 
 const IssueList: React.FC<{ sprintId: string; issues: IssueType[] }> = ({
   sprintId,
@@ -43,76 +41,11 @@ const IssueList: React.FC<{ sprintId: string; issues: IssueType[] }> = ({
 
       <EmtpyIssue
         data-state={isCreating ? "open" : "closed"}
+        className="[&[data-state=closed]]:hidden"
         onCreate={() => setIsCreating(false)}
         onCancel={() => setIsCreating(false)}
-        className="[&[data-state=closed]]:hidden"
       />
     </AccordionContent>
-  );
-};
-
-const EmtpyIssue: React.FC<{
-  className?: string;
-  onCreate: () => void;
-  onCancel: () => void;
-}> = ({ onCreate, onCancel, className, ...props }) => {
-  const [name, setName] = useState("");
-  const [type, setType] = useState<IssueType["type"]>("TASK");
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    focusInput();
-  }, [props]);
-
-  function focusInput() {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }
-
-  function handleCreateIssue(e: React.KeyboardEvent<HTMLInputElement>) {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      onCreate();
-      // createissue
-    }
-  }
-
-  return (
-    <div
-      {...props}
-      className={clsx(
-        "relative flex items-center gap-x-2 border-2 border-blue-400 bg-white p-1.5",
-        className
-      )}
-    >
-      <IssueSelectType
-        currentType={type}
-        dropdownIcon
-        onSelect={(type) => {
-          setType(type);
-          setTimeout(() => focusInput(), 50);
-        }}
-      />
-      <input
-        ref={inputRef}
-        autoFocus
-        type="text"
-        placeholder="What needs to be done?"
-        className=" w-full pl-2 pr-20 text-sm focus:outline-none"
-        value={name}
-        onChange={(e) => setName(e.currentTarget.value)}
-        onKeyDown={handleCreateIssue}
-      />
-      <div className="absolute right-2 z-10 flex gap-x-1">
-        <Button className="aspect-square shadow-md" onClick={() => onCancel()}>
-          <MdClose className="text-sm" />
-        </Button>
-        <Button className="aspect-square shadow-md" onClick={() => onCreate()}>
-          <MdCheck className="text-sm" />
-        </Button>
-      </div>
-    </div>
   );
 };
 
