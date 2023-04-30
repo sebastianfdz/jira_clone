7d171b1300cee574ca3c30243b38aa2723a3194f
sebastianfdz <fdz.seb@gmail.com>
2023-04-30 15:56:39 -0600

    Add post and get all

---
+++ app/api/sprints/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/server/db";
+import { type Sprint } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export type PostSprintResponse = {
+  sprint: Sprint;
+};
+
+export type GetSprintsResponse = {
+  sprints: Sprint[];
+};
+
+export async function POST() {
+  const sprints = await prisma.sprint.findMany();
+  const k = sprints.length + 1;
+
+  const sprint = await prisma.sprint.create({
+    data: {
+      name: `SPRINT-${k}`,
+    },
+  });
+  return NextResponse.json<PostSprintResponse>({ sprint });
+}
+
+export async function GET() {
+  const sprints = await prisma.sprint.findMany();
+  return NextResponse.json<GetSprintsResponse>({ sprints });
+}
