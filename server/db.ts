706d85919f24d9fe4029031e3353766168465dd8
sebastianfdz <fdz.seb@gmail.com>
2023-04-30 15:59:14 -0600

    Add prisma global UserType

--- server/db.ts
+++ server/db.ts
@@ -2,6 +2,18 @@ import { PrismaClient } from "@prisma/client";
 
 import { env } from "@/env.mjs";
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace PrismaJson {
+    type UserType = {
+      id: string;
+      name: string;
+      email: string;
+      avatar: string;
+    };
+  }
+}
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
