9c0aa34496286da6c546eecf098511c24bd11689
sebastianfdz <fdz.seb@gmail.com>
2023-04-30 15:43:33 -0600

    Add zod schema for body validation and return error on validation fail

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,32 +1,44 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/server/db";
-import { type Issue, type IssueType } from "@prisma/client";
+import { type Issue } from "@prisma/client";
+import { z } from "zod";
 
-export type GetIssuesResponse = {
-  issues: Issue[];
-};
+const postSchema = z.object({
+  name: z.string(),
+  type: z.enum(["BUG", "STORY", "TASK", "EPIC"]),
+  sprintId: z.string().nullable(),
+  reporter: z.object({
+    id: z.string(),
+    name: z.string(),
+    email: z.string().email(),
+    avatar: z.string().url(),
+  }),
+  listPosition: z.number(),
+});
 
-export type PostIssueBody = {
-  name: string;
-  type: IssueType;
-  sprintId: Issue["sprintId"];
-  reporter: string;
-};
-
-export type PostIssueResponse = {
-  issue: Issue;
-};
+export type PostIssueBody = z.infer<typeof postSchema>;
+export type GetIssuesResponse = { issues: Issue[] };
+export type PostIssueResponse = { issue: Issue };
 
 export async function GET() {
   const issues = await prisma.issue.findMany();
   const active_issues = issues.filter((issue) => !issue.isDeleted);
-  return NextResponse.json({ issues: active_issues });
+  return NextResponse.json<GetIssuesResponse>({ issues: active_issues });
 }
 
 export async function POST(req: NextRequest) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const body = await req.json();
-  const { name, type, reporter, sprintId } = body as PostIssueBody;
+
+  const validated = postSchema.safeParse(body);
+
+  if (!validated.success) {
+    const message =
+      "Invalid body. " + (validated.error.errors[0]?.message ?? "");
+    return new Response(message, { status: 400 });
+  }
+
+  const { name, type, reporter, sprintId, listPosition } = validated.data;
 
   const issues = await prisma.issue.findMany();
   const k = issues.length + 1;
@@ -38,7 +50,9 @@ export async function POST(req: NextRequest) {
       type,
       reporter,
       sprintId,
+      listPosition,
     },
   });
-  return NextResponse.json({ issue });
+
+  return NextResponse.json<PostIssueResponse>({ issue });
 }
