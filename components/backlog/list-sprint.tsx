3680db41f7e709e264601b8de080a87dfd274961
sebastianfdz <fdz.seb@gmail.com>
2023-04-30 16:01:54 -0600

    Replace type with prisma client yype

--- components/backlog/list-sprint.tsx
+++ components/backlog/list-sprint.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Fragment, useState } from "react";
-import { type IssueType } from "./issue";
 import {
   Accordion,
   AccordionItem,
@@ -12,30 +11,33 @@ import { Button } from "@/components/ui/button";
 import { IssueList } from "./issue-list";
 import { getIssueCountByStatus } from "@/utils/helpers";
 import { IssueStatusCount } from "./issue-status-count";
+import { type Issue as IssueType, type Sprint } from "@prisma/client";
 
-const SprintList: React.FC<{ id: string; issues: IssueType[] }> = ({
-  id,
-  issues,
-}) => {
+const SprintList: React.FC<{
+  sprint: Sprint;
+  issues: IssueType[] | undefined;
+}> = ({ sprint, issues }) => {
   return (
     <Fragment>
-      <Accordion
-        className="rounded-lg bg-zinc-100 p-2"
-        type="single"
-        collapsible
-      >
-        <AccordionItem value={id}>
-          <SprintListHeader sprintId={id} issues={issues} />
-          <IssueList sprintId={id} issues={issues} />
-        </AccordionItem>
-      </Accordion>
+      {issues ? (
+        <Accordion
+          className="rounded-lg bg-zinc-100 p-2"
+          type="single"
+          collapsible
+        >
+          <AccordionItem value={sprint.id}>
+            <SprintListHeader sprint={sprint} issues={issues} />
+            <IssueList sprintId={sprint.id} issues={issues} />
+          </AccordionItem>
+        </Accordion>
+      ) : null}
     </Fragment>
   );
 };
 
-const SprintListHeader: React.FC<{ issues: IssueType[]; sprintId: string }> = ({
+const SprintListHeader: React.FC<{ issues: IssueType[]; sprint: Sprint }> = ({
   issues,
-  sprintId,
+  sprint,
 }) => {
   const [statusCount] = useState(() => getIssueCountByStatus(issues));
 
@@ -48,8 +50,7 @@ const SprintListHeader: React.FC<{ issues: IssueType[]; sprintId: string }> = ({
             aria-hidden
           />
           <div className="flex items-center gap-x-2">
-            <div className="text-semibold whitespace-nowrap">{sprintId}</div>
-            <div className="text-semibold whitespace-nowrap">Sprint 5</div>
+            <div className="text-semibold whitespace-nowrap">{sprint.name}</div>
             <div className="whitespace-nowrap font-normal text-gray-500">
               10 Mar - 17 Mar ({issues.length} issues)
             </div>
