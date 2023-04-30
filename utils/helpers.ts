868af0c878c7d85f1e274566c0dcece8d538da33
sebastianfdz <fdz.seb@gmail.com>
2023-04-29 23:06:44 -0600

    Add get headers function

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,6 +1,18 @@
 import { type IssueType } from "@/components/backlog/issue";
 import { type IsseCountType } from "./types";
 
+export const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
+export const getHeaders = () => {
+  return {
+    "Content-type": "application/json",
+  };
+};
+
 export const moveItemWithinArray = (
   arr: unknown[],
   oldIndex: number,
