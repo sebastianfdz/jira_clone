9aeae1e32bcd25a72a856fab0a6b55da18d24e9e
sebastianfdz <fdz.seb@gmail.com>
2023-04-29 23:05:29 -0600

    Remove project_id page route

--- app/project/backlog/page.tsx
+++ app/project/backlog/page.tsx
@@ -2,21 +2,14 @@ import { Backlog } from "@/components/backlog";
 import { prisma } from "@/server/db";
 import { notFound } from "next/navigation";
 
-const getProject = async (key: string | undefined) => {
-  if (!key) return null;
+const getProject = async () => {
   return await prisma.project.findUnique({
-    where: { key },
+    where: { key: "CLONE" },
   });
 };
 
-const BacklogPage = async ({
-  params,
-}: {
-  params: { [param: string]: string };
-}) => {
-  const { project_key } = params;
-  const project = await getProject(project_key);
-
+const BacklogPage = async () => {
+  const project = await getProject();
   if (!project) return notFound();
 
   return <Backlog project={project} />;
---
+++ app/project/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+const Redirect = () => {
+  redirect("/project/backlog");
+};
+
+export default Redirect;
