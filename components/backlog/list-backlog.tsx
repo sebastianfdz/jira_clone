bfb9ad0d1f123fc34afd473779623d73e570a2fc
sebastianfdz <fdz.seb@gmail.com>
2023-04-30 16:45:40 -0600

    Issues cannot be undefined

--- components/backlog/list-backlog.tsx
+++ components/backlog/list-backlog.tsx
@@ -1,48 +1,61 @@
 "use client";
 import { Fragment, useEffect, useState } from "react";
-import { type IssueType } from "./issue";
 import {
   Accordion,
   AccordionItem,
   AccordionTrigger,
-} from "@radix-ui/react-accordion";
+} from "@/components/ui/accordion";
 import { FaChevronRight } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { IssueList } from "./issue-list";
 import { getIssueCountByStatus } from "@/utils/helpers";
 import { IssueStatusCount } from "./issue-status-count";
+import { api } from "@/utils/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { type Issue as IssueType } from "@prisma/client";
 
-const BacklogList: React.FC<{ id: string; issues: IssueType[] }> = ({
-  id,
-  issues,
-}) => {
+const BacklogList: React.FC<{
+  id: string;
+  issues: IssueType[];
+}> = ({ id, issues }) => {
   const [openAccordion, setOpenAccordion] = useState("");
 
   useEffect(() => {
-    // Open accordion on mount in order for DND to work.
-    setOpenAccordion(`backlog`);
+    setOpenAccordion(`backlog`); // Open accordion on mount in order for DND to work.
   }, [id]);
 
   return (
-    <Fragment>
-      <Accordion
-        className="rounded-md pl-2"
-        type="single"
-        value={openAccordion}
-        onValueChange={setOpenAccordion}
-        collapsible
-      >
-        <AccordionItem value={`backlog`}>
-          <BacklogListHeader issues={issues} />
-          <IssueList sprintId={id} issues={issues} />
-        </AccordionItem>
-      </Accordion>
-    </Fragment>
+    <Accordion
+      className="rounded-md pl-2"
+      type="single"
+      value={openAccordion}
+      onValueChange={setOpenAccordion}
+      collapsible
+    >
+      <AccordionItem value={`backlog`}>
+        <BacklogListHeader issues={issues ?? []} />
+        <IssueList sprintId={null} issues={issues ?? []} />
+      </AccordionItem>
+    </Accordion>
   );
 };
 
 const BacklogListHeader: React.FC<{ issues: IssueType[] }> = ({ issues }) => {
-  const [statusCount] = useState(() => getIssueCountByStatus(issues));
+  const [statusCount] = useState(() => getIssueCountByStatus(issues ?? []));
+  const queryClient = useQueryClient();
+  const { mutate: createSprint } = useMutation(
+    ["sprints"],
+    api.sprints.postSprint
+  );
+
+  function handleCreateSprint() {
+    createSprint(undefined, {
+      onSuccess: () => {
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        queryClient.invalidateQueries(["sprints"]);
+      },
+    });
+  }
 
   return (
     <div className="flex w-full items-center justify-between text-sm ">
@@ -62,7 +75,7 @@ const BacklogListHeader: React.FC<{ issues: IssueType[] }> = ({ issues }) => {
       </AccordionTrigger>
       <div className="flex items-center gap-x-2 py-2">
         <IssueStatusCount statusCount={statusCount} />
-        <Button>
+        <Button onClick={handleCreateSprint}>
           <span className="whitespace-nowrap">Create Sprint</span>
         </Button>
       </div>
