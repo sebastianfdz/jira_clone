b3d0c0ae81c5476d9ca6c9792bfb5d1e3d2de213
sebastianfdz <fdz.seb@gmail.com>
2023-04-30 22:03:00 -0600

    Import top nav bar

--- app/project/layout.tsx
+++ app/project/layout.tsx
@@ -1,12 +1,12 @@
+"use client";
 import { Sidebar } from "@/components/sidebar";
+import { TopNavbar } from "@/components/top-navbar";
 import { Fragment } from "react";
 
 const ProjectLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Fragment>
-      <div className="flex h-12 w-full items-center justify-center border-b">
-        top nav bar
-      </div>
+      <TopNavbar />
       <div className="flex h-[calc(100vh_-_3rem)] w-full pb-5">
         <Sidebar />
         <main className="w-full">{children}</main>
