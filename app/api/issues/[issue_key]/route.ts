f433ab4dab83d8437b6d99d36aa0a3b199d508f7
sebastianfdz <fdz.seb@gmail.com>
2023-04-30 22:04:29 -0600

    Add path issue endpoint

--- app/api/issues/[issue_key]/route.ts
+++ app/api/issues/[issue_key]/route.ts
@@ -1,6 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/server/db";
-import { type Issue } from "@prisma/client";
+import { IssueStatus, type Issue, IssueType } from "@prisma/client";
+import { z } from "zod";
 
 export type GetIssueDetailsResponse = {
   issue: Issue | null;
@@ -17,5 +18,97 @@ export async function GET(
     },
   });
   // return NextResponse.json<GetIssueDetailsResponse>({ issue });
+  return NextResponse.json({ issue });
+}
+const patchSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  type: z.nativeEnum(IssueType).optional(),
+  status: z.nativeEnum(IssueStatus).optional(),
+  listPosition: z.number().optional(),
+  reporter: z
+    .object({
+      id: z.string(),
+      name: z.string(),
+      email: z.string().email(),
+      avatar: z.string().url(),
+    })
+    .optional(),
+  assignee: z
+    .object({
+      id: z.string(),
+      name: z.string(),
+      email: z.string().email(),
+      avatar: z.string().url(),
+    })
+    .optional(),
+  parentId: z.string().optional(),
+  sprintId: z.string().optional(),
+  isDeleted: z.boolean().optional(),
+});
+
+export type PatchIssueBody = z.infer<typeof patchSchema>;
+
+type PatchParams = {
+  params: {
+    issue_key: string;
+  };
+};
+
+export async function PATCH(req: NextRequest, { params }: PatchParams) {
+  const { issue_key } = params;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const body = await req.json();
+  const validated = patchSchema.safeParse(body);
+
+  if (!validated.success) {
+    const message =
+      "Invalid body. " + (validated.error.errors[0]?.message ?? "");
+    return new Response(message, { status: 400 });
+  }
+
+  const {
+    name,
+    description,
+    type,
+    status,
+    listPosition,
+    reporter,
+    assignee,
+    isDeleted,
+    sprintId,
+    parentId,
+  } = validated.data;
+
+  const current = await prisma.issue.findUnique({
+    where: {
+      key: issue_key,
+    },
+  });
+
+  if (!current) {
+    return new Response("Issue not found", { status: 404 });
+  }
+
+  const issue = await prisma.issue.update({
+    where: {
+      key: issue_key,
+    },
+    data: {
+      name: name ?? current.name,
+      description: description ?? current.description,
+      status: status ?? current.status,
+      type: type ?? current.type,
+      listPosition: listPosition ?? current.listPosition,
+      reporter: reporter ?? current.reporter,
+      assignee: assignee ?? current.assignee ?? undefined,
+      isDeleted: isDeleted ?? current.isDeleted,
+      sprintId: sprintId ?? current.sprintId,
+      parentId: parentId ?? current.parentId,
+    },
+  });
+
+  // return NextResponse.json<PostIssueResponse>({ issue });
   return NextResponse.json({ issue });
 }
