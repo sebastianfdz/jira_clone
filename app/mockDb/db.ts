cfad9d031f4f31e40c5fae94be2c10069ad9467f
sebastianfdz <fdz.seb@gmail.com>
2023-05-10 18:24:35 -0600

    Add mock db

---
+++ app/mockDb/db.ts
@@ -0,0 +1,25 @@
+import { type StatusObject } from "@/components/issue-select-status";
+
+export const statuses: StatusObject[] = [
+  {
+    value: "TODO",
+    smBgColor: "#d4d4d8",
+    lgBgColor: "#d4d4d8",
+    smTextColor: "#4b5563",
+    lgTextColor: "#4b5563",
+  },
+  {
+    value: "IN_PROGRESS",
+    smBgColor: "#e0ecfc",
+    lgBgColor: "#0854cc",
+    smTextColor: "#0854cc",
+    lgTextColor: "#fff",
+  },
+  {
+    value: "DONE",
+    smBgColor: "#e8fcec",
+    lgBgColor: "#08845c",
+    smTextColor: "#08845c",
+    lgTextColor: "#fff",
+  },
+];
