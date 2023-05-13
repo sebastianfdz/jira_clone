b3238a00f80fa70c8b5e30e1cf4a6de25d6cbe81
sebastianfdz <fdz.seb@gmail.com>
2023-05-12 18:22:34 -0600

    Remove unused helpers

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,51 +1,51 @@
 import { type IssueCountType } from "./types";
-import { type Issue as IssueType } from "@prisma/client";
+import { type IssueType } from "@/utils/types";
 
-export const getBaseUrl = () => {
+export function getBaseUrl() {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
+}
 
-export const getHeaders = () => {
+export function getHeaders() {
   return {
     "Content-type": "application/json",
   };
-};
+}
 
-export const moveItemWithinArray = (
+export function moveItemWithinArray(
   arr: unknown[],
   oldIndex: number,
   newIndex: number
-) => {
+) {
   const arrClone = [...arr];
   arrClone.splice(newIndex, 0, arrClone.splice(oldIndex, 1)[0]);
   return arrClone;
-};
+}
 
-export const insertItemIntoArray = (
+export function insertItemIntoArray(
   arr: unknown[],
   item: unknown,
   index: number
-) => {
+) {
   // Snippet taken from https://github.com/oldboyxx/jira_clone
   const arrClone = [...arr];
   arrClone.splice(index, 0, item);
   return arrClone;
-};
+}
 
-export const capitalize = (str: string) => {
+export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-};
+}
 
-export const capitalizeMany = (str: string) => {
+export function capitalizeMany(str: string) {
   return str
     .split(" ")
     .map((word) => capitalize(word))
     .join(" ");
-};
+}
 
-export const getIssueCountByStatus = (issues: IssueType[]) => {
+export function getIssueCountByStatus(issues: IssueType[]) {
   return issues.reduce(
     (acc, issue) => {
       acc[issue.status]++;
@@ -57,12 +57,9 @@ export const getIssueCountByStatus = (issues: IssueType[]) => {
       DONE: 0,
     } as IssueCountType
   );
-};
+}
 
-export const hexToRgba = (hex: string, opacity: number) => {
-  const r = parseInt(hex.slice(1, 3), 16);
-  const g = parseInt(hex.slice(3, 5), 16);
-  const b = parseInt(hex.slice(5, 7), 16);
-
-  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
-};
+export function isEpic(issue: IssueType | IssueType["parent"] | null) {
+  if (!issue) return false;
+  return issue.type == "EPIC";
+}
