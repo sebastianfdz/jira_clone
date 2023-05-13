bb75646d450d988a8bf518ae485f35a70de1ae29
sebastianfdz <fdz.seb@gmail.com>
2023-05-13 14:41:47 -0600

    Update issue on drag end

--- components/backlog/list-group.tsx
+++ components/backlog/list-group.tsx
@@ -5,41 +5,22 @@ import clsx from "clsx";
 import { api } from "@/utils/api";
 import { BacklogList } from "./list-backlog";
 import { SprintList } from "./list-sprint";
-import { insertItemIntoArray, moveItemWithinArray } from "@/utils/helpers";
 import { type IssueType } from "@/utils/types";
-import {
-  DragDropContext,
-  type DraggableLocation,
-  type DropResult,
-} from "react-beautiful-dnd";
+import { DragDropContext, type DropResult } from "react-beautiful-dnd";
+import { isNullish, sprintId } from "@/utils/helpers";
 
 const ListGroup: React.FC<{ className?: string }> = ({ className }) => {
-  const { issues } = useIssues();
+  const { issues, updateIssue } = useIssues();
   const { data: sprints } = useQuery(["sprints"], api.sprints.getSprints);
 
   const onDragEnd = (result: DropResult) => {
-    const { destination, source } = result;
-    if (!positionHasChanged(source, destination)) return;
-    if (source.droppableId === destination?.droppableId) {
-      // setIssues((prev) => {
-      //   return moveIssueWithinColumn(
-      //     prev,
-      //     source.droppableId,
-      //     source.index,
-      //     destination.index
-      //   );
-      // });
-    } else if (destination) {
-      // setIssues((prev) => {
-      //   return moveIssueBetweenColumns(
-      //     prev,
-      //     source.droppableId,
-      //     destination.droppableId,
-      //     source.index,
-      //     destination.index
-      //   );
-      // });
-    }
+    const { destination } = result;
+    if (isNullish(destination)) return;
+    updateIssue({
+      issue_key: result.draggableId,
+      sprintId: sprintId(destination.droppableId),
+      listPosition: destination.index,
+    });
   };
 
   if (!issues || !sprints) return <div />;
@@ -62,92 +43,10 @@ const ListGroup: React.FC<{ className?: string }> = ({ className }) => {
 
 ListGroup.displayName = "ListGroup";
 
-function sprintId(id: string) {
-  return id == "backlog" ? null : id;
-}
-
 function filterOutEpics(issues: IssueType[], sprintId: string | null) {
   return issues.filter(
     (issue) => issue.sprintId === sprintId && issue.type !== "EPIC"
   );
-}
-
-function moveIssueWithinColumn(
-  issues: IssueType[],
-  columnId: string,
-  startIndex: number,
-  endIndex: number
-) {
-  const unaffectedIssues = issues.filter(
-    (issue) => issue.sprintId !== sprintId(columnId)
-  );
-  const affectedIssues = getSortedListIssues(issues, columnId);
-  const reordered = moveItemWithinArray(
-    affectedIssues,
-    startIndex,
-    endIndex
-  ) as IssueType[];
-
-  return [
-    ...unaffectedIssues,
-    ...reordered.map((issue, index) => ({ ...issue, listPosition: index })),
-  ];
-}
-
-function moveIssueBetweenColumns(
-  issues: IssueType[],
-  startColumnId: string,
-  endColumnId: string,
-  startIndex: number,
-  endIndex: number
-) {
-  // TODO
-  const unaffectedIssues = issues.filter(
-    (issue) =>
-      issue.sprintId !== sprintId(startColumnId) &&
-      issue.sprintId !== sprintId(endColumnId)
-  );
-
-  const startColumn = getSortedListIssues(issues, startColumnId);
-  const itemToInsert = {
-    ...startColumn[startIndex],
-    sprint: sprintId(endColumnId),
-  };
-  startColumn.splice(startIndex, 1);
-
-  const endColumn = getSortedListIssues(issues, endColumnId);
-  const newEndColumn = insertItemIntoArray(
-    endColumn,
-    itemToInsert,
-    endIndex
-  ) as IssueType[];
-
-  return [
-    ...unaffectedIssues,
-    ...startColumn.map((issue, index) => ({
-      ...issue,
-      listPosition: index,
-    })),
-    ...newEndColumn.map((issue, index) => ({ ...issue, listPosition: index })),
-  ] as IssueType[];
-}
-
-function positionHasChanged(
-  source: DraggableLocation,
-  destination: DraggableLocation | null | undefined
-) {
-  // Snippet taken from https://github.com/oldboyxx/jira_clone
-  if (!destination) return false;
-  const isSameList = destination.droppableId === source.droppableId;
-  const isSamePosition = destination.index === source.index;
-  return !isSameList || !isSamePosition;
-}
-
-function getSortedListIssues(issues: IssueType[], listId: string) {
-  const clone = [...issues];
-  return clone
-    .filter((issue) => issue.sprintId === sprintId(listId))
-    .sort((a, b) => a.listPosition - b.listPosition);
 }
 
 export { ListGroup };
