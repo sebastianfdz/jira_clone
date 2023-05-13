9f66d76f6462468fe5988e4e04629ef8f7f48134
sebastianfdz <fdz.seb@gmail.com>
2023-05-12 18:51:00 -0600

    Extend colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        task: "#4bade8",
+        story: "#68bc3c",
+        bug: "#e84c3c",
+        epic: "#984ce4",
+        inprogress: "#0854cc",
+        done: "#08845c",
+        todo: "#d4d4d8",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
