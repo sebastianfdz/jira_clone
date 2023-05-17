ce37bd3ec4494c65225b1f5439ecc102c82e41fa
sebastianfdz <fdz.seb@gmail.com>
2023-05-16 18:35:35 -0600

    Move header to backlog index

--- app/project/backlog/layout.tsx
+++ app/project/backlog/layout.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { BacklogHeader } from "@/components/backlog/header";
 import { Container } from "@/components/ui/container";
 import { SelectedIssueProvider } from "@/hooks/useSelectedIssue";
 
 const BacklogLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Container className="h-full">
-      <BacklogHeader />
       <main className="w-full">
         <SelectedIssueProvider>{children}</SelectedIssueProvider>
       </main>
