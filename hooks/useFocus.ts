d216c51c852f136a9355a13b0198876f3d16a996
sebastianfdz <fdz.seb@gmail.com>
2023-05-16 18:28:49 -0600

    Add usefocus hook

---
+++ hooks/useFocus.ts
@@ -0,0 +1,32 @@
+import { useRef, useEffect, useState } from "react";
+
+export const useFocus = (): [React.Ref<HTMLInputElement>, boolean] => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    const handleFocus = () => {
+      setIsFocused(true);
+    };
+
+    const handleBlur = () => {
+      setIsFocused(false);
+    };
+
+    const inputElement = inputRef.current;
+
+    if (inputElement) {
+      inputElement.addEventListener("focus", handleFocus);
+      inputElement.addEventListener("blur", handleBlur);
+    }
+
+    return () => {
+      if (inputElement) {
+        inputElement.removeEventListener("focus", handleFocus);
+        inputElement.removeEventListener("blur", handleBlur);
+      }
+    };
+  }, []);
+
+  return [inputRef, isFocused];
+};
