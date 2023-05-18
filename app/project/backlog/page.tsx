57620c898fe28582fc983e0fca8f3d8de88e80d0
sebastianfdz <fdz.seb@gmail.com>
2023-05-17 20:41:34 -0600

    Use client

--- app/project/backlog/page.tsx
+++ app/project/backlog/page.tsx
@@ -1,29 +1,12 @@
 "use client";
 import { Backlog } from "@/components/backlog";
-// import { prisma } from "@/server/db";
-import { api } from "@/utils/api";
-import { useQuery } from "@tanstack/react-query";
 import { notFound } from "next/navigation";
 import BacklogSkeleton from "./loading";
-
-// const getProject = async () => {
-//   return await prisma.project.findUnique({
-//     where: { key: "CLONE" },
-//   });
-// };
+import { useProject } from "@/hooks/useProject";
 
 const BacklogPage = () => {
-  // const project = await getProject();
-  const { data: project, isLoading } = useQuery(
-    ["project"],
-    api.project.getProject,
-    {
-      onError: (error) => {
-        console.log("error", error);
-      },
-    }
-  );
-  if (isLoading) return <BacklogSkeleton />;
+  const { project, projectIsLoading } = useProject();
+  if (projectIsLoading) return <BacklogSkeleton />;
   if (!project) return notFound();
   return <Backlog project={project} />;
 };
