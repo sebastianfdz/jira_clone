e3338729383d95a4075664c5cf7901641ab3f51b
sebastianfdz <fdz.seb@gmail.com>
2023-05-17 20:06:45 -0600

    Return project members

---
+++ hooks/useProject.ts
@@ -0,0 +1,22 @@
+import { api } from "@/utils/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useProject = () => {
+  const { data: project, isLoading: projectIsLoading } = useQuery(
+    ["project"],
+    api.project.getProject
+  );
+  const { data: members } = useQuery(
+    ["project-members"],
+    () => api.project.getMembers({ project_id: project?.id ?? "" }),
+    {
+      enabled: !!project?.id,
+    }
+  );
+
+  return {
+    project,
+    projectIsLoading,
+    members,
+  };
+};
