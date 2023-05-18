c274686fdebe0420a07ad5fa41816fdc3c387291
sebastianfdz <fdz.seb@gmail.com>
2023-05-17 20:40:30 -0600

    Add style and context to members and search

--- components/backlog/header.tsx
+++ components/backlog/header.tsx
@@ -4,19 +4,68 @@ import { useFocus } from "@/hooks/useFocus";
 import { BsX } from "react-icons/bs";
 import { MdSearch } from "react-icons/md";
 import debounce from "lodash.debounce";
+import { useProject } from "@/hooks/useProject";
+import { Avatar } from "../avatar";
+import { Button } from "../ui/button";
+import { useFiltersContext } from "@/context/useFilters";
+import { type Project } from "@prisma/client";
 
-const BacklogHeader: React.FC<{
-  search: string;
-  setSearch: React.Dispatch<React.SetStateAction<string>>;
-}> = ({ search, setSearch }) => {
+const BacklogHeader: React.FC<{ project: Project }> = ({ project }) => {
+  const { search, setSearch } = useFiltersContext();
   return (
     <div className="flex h-fit flex-col">
-      <div className="text-sm text-gray-500">Projects / Clone</div>
+      <div className="text-sm text-gray-500">Projects / {project.name}</div>
       <h1>Backlog </h1>
       <div className="my-3 flex items-center gap-x-5">
         <SearchBar search={search} setSearch={setSearch} />
-        [members][add_member][epic_filter][type_filter]
+        <Members />
+        [add_member][epic_filter][type_filter]
       </div>
+    </div>
+  );
+};
+
+const Members = () => {
+  const { members } = useProject();
+  const { assignees, setAssignees } = useFiltersContext();
+  const unassigned = {
+    id: "unassigned",
+    name: "Unassigned",
+    avatar: "",
+    email: "",
+  };
+
+  function handleAssigneeFilter(id: string) {
+    setAssignees((prev) => {
+      if (prev.includes(id)) return prev.filter((a) => a !== id);
+      return [...prev, id];
+    });
+  }
+  if (!members) return <div />;
+
+  return (
+    <div className="flex">
+      {[...members, unassigned].map((member, index) => {
+        return (
+          <div
+            key={member.id}
+            style={{ zIndex: 10 - index }}
+            className="hover:!z-10"
+          >
+            <Button
+              onClick={() => handleAssigneeFilter(member.id)}
+              customColors
+              customPadding
+              data-state={
+                assignees.includes(member.id) ? "selected" : "not-selected"
+              }
+              className="-mx-1 flex border-spacing-2 rounded-full border-2 border-transparent bg-white p-0.5 transition-all duration-300 hover:-mt-1.5 [&[data-state=selected]]:border-inprogress"
+            >
+              <Avatar size={35} src={member.avatar} alt={`${member.name}`} />
+            </Button>
+          </div>
+        );
+      })}
     </div>
   );
 };
