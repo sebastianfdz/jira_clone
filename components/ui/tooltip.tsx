abd093deaa893775f5960c60d6e659294a9d4760
sebastianfdz <fdz.seb@gmail.com>
2023-05-17 18:20:47 -0600

    Add tooltip component

---
+++ components/ui/tooltip.tsx
@@ -0,0 +1,96 @@
+import React, { type ReactNode } from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import clsx from "clsx";
+
+const Tooltip = TooltipPrimitive.Root;
+
+type TooltipTriggerProps = React.ComponentProps<
+  typeof TooltipPrimitive.Trigger
+>;
+type TooltipTriggerRef = React.ElementRef<typeof TooltipPrimitive.Trigger>;
+
+const TooltipTrigger = React.forwardRef<TooltipTriggerRef, TooltipTriggerProps>(
+  ({ children, className, ...props }, forwardedRef) => (
+    <TooltipPrimitive.Trigger
+      className={clsx("", className)}
+      {...props}
+      ref={forwardedRef}
+    >
+      {children}
+    </TooltipPrimitive.Trigger>
+  )
+);
+
+TooltipTrigger.displayName = "TooltipTrigger";
+
+type TooltipPortalProps = React.ComponentProps<typeof TooltipPrimitive.Portal>;
+
+const TooltipPortal: React.FC<TooltipPortalProps> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <TooltipPrimitive.Portal className={clsx("", className)} {...props}>
+    {children}
+  </TooltipPrimitive.Portal>
+);
+
+TooltipPortal.displayName = "TooltipPortal";
+
+type TooltipContentProps = React.ComponentProps<
+  typeof TooltipPrimitive.Content
+>;
+type TooltipContentRef = React.ElementRef<typeof TooltipPrimitive.Content>;
+
+const TooltipContent = React.forwardRef<TooltipContentRef, TooltipContentProps>(
+  ({ children, className, ...props }, forwardedRef) => (
+    <TooltipPrimitive.Content
+      className={clsx("", className)}
+      {...props}
+      ref={forwardedRef}
+    >
+      {children}
+    </TooltipPrimitive.Content>
+  )
+);
+
+TooltipContent.displayName = "TooltipContent";
+
+type TooltipArrowProps = React.ComponentProps<typeof TooltipPrimitive.Arrow>;
+
+const TooltipArrow: React.FC<TooltipArrowProps> = ({ children, ...props }) => (
+  <TooltipPrimitive.Arrow {...props}>{children}</TooltipPrimitive.Arrow>
+);
+
+TooltipArrow.displayName = "TooltipArrow";
+
+const TooltipWrapper: React.FC<
+  {
+    children: ReactNode;
+    text: string;
+  } & TooltipContentProps
+> = ({ children, text, side = "bottom" }) => {
+  return (
+    <Tooltip delayDuration={100}>
+      <TooltipTrigger asChild>{children}</TooltipTrigger>
+      <TooltipPortal>
+        <TooltipContent
+          side={side}
+          sideOffset={4}
+          className="rounded-sm bg-black bg-opacity-80 px-1.5 py-0.5 text-xs text-white"
+        >
+          {text}
+        </TooltipContent>
+      </TooltipPortal>
+    </Tooltip>
+  );
+};
+
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipPortal,
+  TooltipTrigger,
+  TooltipArrow,
+  TooltipWrapper,
+};
