79d2ad86708326cbfd97b27f6b0288cb580549c4
sebastianfdz <fdz.seb@gmail.com>
2023-05-19 12:11:10 -0600

    Add editing check to listener

--- hooks/useKeydownListener.ts
+++ hooks/useKeydownListener.ts
@@ -3,33 +3,39 @@ import { useRef, useEffect, type RefObject } from "react";
 export const useKeydownListener = (
   ref: RefObject<HTMLElement>,
   targetKey: string | string[],
-  callback: (ref: RefObject<HTMLElement>) => void
+  callback: (ref: RefObject<HTMLElement>, event?: KeyboardEvent) => void
 ) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const isInsideContentEditable = () =>
+    !!document.activeElement?.getAttribute("contenteditable") ||
+    (document.activeElement?.tagName &&
+      ["TEXTAREA", "INPUT"].includes(document.activeElement?.tagName));
+
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (typeof targetKey === "string") {
-        if (event.key === targetKey) {
-          event.preventDefault();
-          callback(ref);
+      if (!isInsideContentEditable())
+        if (typeof targetKey === "string") {
+          if (event.key === targetKey) {
+            event.preventDefault();
+            callback(ref, event);
+          }
+        } else if (Array.isArray(targetKey)) {
+          if (targetKey.includes(event.key)) {
+            event.preventDefault();
+            callback(ref, event);
+          }
         }
-      } else if (Array.isArray(targetKey)) {
-        if (targetKey.includes(event.key)) {
-          event.preventDefault();
-          callback(ref);
-        }
-      }
     };
 
     const currentRef = ref.current;
     if (currentRef) {
-      window.addEventListener("keydown", handleKeyDown);
+      document.addEventListener("keydown", handleKeyDown);
     }
 
     return () => {
       if (currentRef) {
-        window.removeEventListener("keydown", handleKeyDown);
+        document.removeEventListener("keydown", handleKeyDown);
       }
     };
   }, [ref, targetKey, callback]);
