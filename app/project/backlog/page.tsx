379d8c2ce7c713d405a6eaf84f50d9f710fd64ec
sebastianfdz <fdz.seb@gmail.com>
2023-05-20 12:14:29 -0600

    Fetch issues and sprints asap

--- app/project/backlog/page.tsx
+++ app/project/backlog/page.tsx
@@ -3,11 +3,20 @@ import { Backlog } from "@/components/backlog";
 import { notFound } from "next/navigation";
 import BacklogSkeleton from "./loading";
 import { useProject } from "@/hooks/useProject";
+import { useIssues } from "@/hooks/useIssues";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/utils/api";
 
 const BacklogPage = () => {
   const { project, projectIsLoading } = useProject();
-  if (projectIsLoading) return <BacklogSkeleton />;
-  if (!project) return notFound();
+  const { issues, issuesLoading } = useIssues(); // fetch asap
+  const { data: sprints, isLoading: sprintsLoading } = useQuery(
+    ["sprints"],
+    api.sprints.getSprints
+  ); // fetch asap
+  if (projectIsLoading || issuesLoading || sprintsLoading)
+    return <BacklogSkeleton />;
+  if (!project || !issues || !sprints) return notFound();
   return <Backlog project={project} />;
 };
 
