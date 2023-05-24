9e3507ebc5e4c08f1f6031fe0c01190eb14e7323
sebastianfdz <fdz.seb@gmail.com>
2023-05-23 22:44:57 -0600

    Add use is in viewport hook

---
+++ hooks/useIsInViewport.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect, useRef } from "react";
+
+type UseIsInViewportOptions = {
+  threshold?: number;
+};
+
+export const useIsInViewport = (
+  options: UseIsInViewportOptions = {}
+): [boolean, React.RefObject<HTMLDivElement>] => {
+  const { threshold = 0 } = options;
+  const [isInViewport, setIsInViewport] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const element = ref.current;
+
+    if (!element) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry) {
+          setIsInViewport(entry.isIntersecting);
+        }
+      },
+      {
+        threshold: threshold,
+      }
+    );
+
+    observer.observe(element);
+
+    return () => {
+      observer.unobserve(element);
+    };
+  }, [threshold]);
+
+  return [isInViewport, ref];
+};
