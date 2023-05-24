47937b42cca0c5f842eb3f6154585f8fa7282545
sebastianfdz <fdz.seb@gmail.com>
2023-05-23 20:37:55 -0600

    Remove padding in y axis

--- components/text-editor/preview.tsx
+++ components/text-editor/preview.tsx
@@ -7,11 +7,13 @@ import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import CodeHighlightPlugin from "./plugins/code-highlight-plugin";
 import { EditorComposer } from "./context/lexical-composer";
 import { type EditorContentType } from "./editor";
+import clsx from "clsx";
 
 export const EditorPreview: React.FC<{
   action: "description" | "comment";
   content: EditorContentType;
-}> = ({ action, content }) => {
+  className?: string;
+}> = ({ action, content, className }) => {
   const [jsonState] = useState<EditorContentType>(content);
 
   return (
@@ -20,10 +22,15 @@ export const EditorPreview: React.FC<{
         <RichTextPlugin
           ErrorBoundary={LexicalErrorBoundary}
           contentEditable={
-            <ContentEditable className="w-full resize-none overflow-hidden text-ellipsis rounded-[3px] p-1 outline-none transition-all duration-200 hover:bg-gray-100" />
+            <ContentEditable
+              className={clsx(
+                "w-full resize-none overflow-hidden text-ellipsis rounded-[3px] outline-none",
+                className
+              )}
+            />
           }
           placeholder={
-            <div className="pointer-events-none  absolute left-0 top-0 flex h-full select-none items-center px-1 text-sm text-gray-500">
+            <div className="pointer-events-none absolute left-0 top-0 flex h-full select-none items-center px-1 text-sm text-gray-500">
               Add your {action} here...
             </div>
           }
