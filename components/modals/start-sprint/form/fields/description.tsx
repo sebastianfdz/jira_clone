80eb4d498ab774386e7c6498ff2b5f5f2f09a972
sebastianfdz <fdz.seb@gmail.com>
2023-05-25 09:05:15 -0600

    Add description field file

---
+++ components/modals/start-sprint/form/fields/description.tsx
@@ -0,0 +1,20 @@
+import { type UseFormRegister } from "react-hook-form";
+import { type FormValues, Label } from "..";
+
+const DescriptionField: React.FC<{
+  register: UseFormRegister<FormValues>;
+}> = ({ register }) => {
+  return (
+    <div className="my-2">
+      <Label htmlFor="description" text="Sprint Goal" required={false} />
+      <input
+        {...register("description")}
+        type="text"
+        id="description"
+        className="form-input block h-32 w-[500px] rounded-[3px] border border-gray-300 px-2 text-sm shadow-sm outline-2 transition-all duration-200 focus:outline-blue-400"
+      />
+    </div>
+  );
+};
+
+export { DescriptionField };
