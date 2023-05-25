f97965b7d7a36731a40d90021aa7266a6cf69afe
sebastianfdz <fdz.seb@gmail.com>
2023-05-25 09:05:07 -0600

    Add name field file

---
+++ components/modals/start-sprint/form/fields/name.tsx
@@ -0,0 +1,27 @@
+import { type FieldErrors, type UseFormRegister } from "react-hook-form";
+import { Error, type FormValues, Label } from "..";
+import clsx from "clsx";
+
+const NameField: React.FC<{
+  errors: FieldErrors<FormValues>;
+  register: UseFormRegister<FormValues>;
+}> = ({ errors, register }) => {
+  return (
+    <div className="my-2">
+      <Label htmlFor="name" text="Sprint Name" />
+      <input
+        {...register("name", { required: true })}
+        aria-invalid={errors.name ? "true" : "false"}
+        type="text"
+        id="name"
+        className={clsx(
+          errors.name ? "focus:outline-red-500" : "focus:outline-blue-400",
+          "form-input block h-10 w-64 rounded-[3px] border border-gray-300 px-2 text-sm shadow-sm outline-2 transition-all duration-200"
+        )}
+      />
+      <Error trigger={errors.name} message="Sprint name is required" />
+    </div>
+  );
+};
+
+export { NameField };
