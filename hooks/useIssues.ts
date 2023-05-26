1bc43dda92cc41ae2a37042b841385e3a0b522db
sebastianfdz <fdz.seb@gmail.com>
2023-05-25 21:13:09 -0600

    Add batch update

--- hooks/useIssues.ts
+++ hooks/useIssues.ts
@@ -22,6 +22,54 @@ export const useIssues = () => {
     }
   );
 
+  // UPDATE BATCH
+  const { mutate: updateIssuesBatch, isLoading: batchUpdating } = useMutation(
+    api.issues.updateBatchIssues,
+    {
+      // OPTIMISTIC UPDATE
+      onMutate: async (newIssue) => {
+        // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+        await queryClient.cancelQueries(["issues"]);
+        // Snapshot the previous value
+        const previousIssues = queryClient.getQueryData<
+          (IssueType | IssueType["parent"])[]
+        >(["issues"]);
+
+        // Optimistically updating the issues
+        queryClient.setQueryData(
+          ["issues"],
+          (old?: (IssueType | IssueType["parent"])[]) => {
+            const newIssues = (old ?? []).map((issue) => {
+              const { keys, ...updatedProps } = newIssue;
+              if (keys.includes(issue.key)) {
+                // Assign the new prop values to the issue
+                return Object.assign(issue, updatedProps);
+              }
+              return issue;
+            });
+            return newIssues;
+          }
+        );
+
+        // Return a context object with the snapshotted value
+        return { previousIssues };
+      },
+      onError: (err, newIssue, context) => {
+        // If the mutation fails, use the context returned from onMutate to roll back
+        queryClient.setQueryData(["issues"], context?.previousIssues);
+        toast.error({
+          message: `Something went wrong while batch updating issues`,
+          description: "Please try again later.",
+        });
+      },
+      onSettled: () => {
+        // Always refetch after error or success
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        queryClient.invalidateQueries(["issues"]);
+      },
+    }
+  );
+
   // UPDATE
   const { mutate: updateIssue, isLoading: isUpdating } = useMutation(
     api.issues.patchIssue,
@@ -202,6 +250,8 @@ export const useIssues = () => {
     issuesLoading,
     updateIssue,
     isUpdating,
+    updateIssuesBatch,
+    batchUpdating,
     createIssue,
     isCreating,
     deleteIssue,
