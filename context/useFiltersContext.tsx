a89211a8094fab64ea50ed60db0a838f6cc074fc
sebastianfdz <fdz.seb@gmail.com>
2023-05-27 18:16:45 -0600

    Add epics and issuetypes to filter context

--- context/useFiltersContext.tsx
+++ context/useFiltersContext.tsx
@@ -6,6 +6,10 @@ type FiltersContextProps = {
   setAssignees: React.Dispatch<React.SetStateAction<string[]>>;
   search: string;
   setSearch: React.Dispatch<React.SetStateAction<string>>;
+  epics: string[];
+  setEpics: React.Dispatch<React.SetStateAction<string[]>>;
+  isseTypes: string[];
+  setIsseTypes: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
 const FiltersContext = createContext<FiltersContextProps>({
@@ -15,14 +19,32 @@ const FiltersContext = createContext<FiltersContextProps>({
   search: "",
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   setSearch: () => {},
+  epics: [],
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setEpics: () => {},
+  isseTypes: [],
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setIsseTypes: () => {},
 });
 
 export const FiltersProvider = ({ children }: { children: ReactNode }) => {
   const [assignees, setAssignees] = useState<string[]>([]);
   const [search, setSearch] = useState<string>("");
+  const [epics, setEpics] = useState<string[]>([]);
+  const [isseTypes, setIsseTypes] = useState<string[]>([]);
+
   return (
     <FiltersContext.Provider
-      value={{ assignees, setAssignees, search, setSearch }}
+      value={{
+        assignees,
+        setAssignees,
+        search,
+        setSearch,
+        epics,
+        setEpics,
+        isseTypes,
+        setIsseTypes,
+      }}
     >
       {children}
     </FiltersContext.Provider>
