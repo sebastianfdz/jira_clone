9aa5e5b2602713136e0bf6c10692538fa3905a86
sebastianfdz <fdz.seb@gmail.com>
2023-05-27 18:20:11 -0600

    Add filter components

--- components/backlog/header.tsx
+++ components/backlog/header.tsx
@@ -1,14 +1,13 @@
 "use client";
-import { useEffect, useMemo, useState } from "react";
-import { useFocus } from "@/hooks/useFocus";
-import { BsX } from "react-icons/bs";
-import { MdSearch } from "react-icons/md";
-import debounce from "lodash.debounce";
+import React from "react";
 import { useProject } from "@/hooks/query-hooks/useProject";
 import { Avatar } from "../avatar";
 import { Button } from "../ui/button";
 import { useFiltersContext } from "@/context/useFiltersContext";
 import { type Project } from "@prisma/client";
+import { EpicFilter } from "./filter-epic";
+import { IssueTypeFilter } from "./filter-issue-type";
+import { SearchBar } from "./filter-search-bar";
 
 const BacklogHeader: React.FC<{ project: Project }> = ({ project }) => {
   const { search, setSearch } = useFiltersContext();
@@ -19,7 +18,9 @@ const BacklogHeader: React.FC<{ project: Project }> = ({ project }) => {
       <div className="my-3 flex items-center gap-x-5">
         <SearchBar search={search} setSearch={setSearch} />
         <Members />
-        [add_member][epic_filter][type_filter]
+        [add_member]
+        <EpicFilter />
+        <IssueTypeFilter />
       </div>
     </div>
   );
@@ -66,63 +67,6 @@ const Members = () => {
           </div>
         );
       })}
-    </div>
-  );
-};
-
-const SearchBar: React.FC<{
-  search: string;
-  setSearch: React.Dispatch<React.SetStateAction<string>>;
-}> = ({ search, setSearch }) => {
-  const [ref, isFocused] = useFocus();
-
-  const [localSearch, setLocalSearch] = useState(search);
-
-  const debouncedSetSearch = useMemo(
-    () => debounce((str: string) => setSearch(str), 350),
-    [setSearch]
-  );
-
-  function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
-    setLocalSearch(e.target.value);
-    debouncedSetSearch(e.target.value);
-  }
-
-  function handleClearSearch() {
-    setLocalSearch("");
-    setSearch("");
-  }
-
-  useEffect(() => {
-    return () => {
-      debouncedSetSearch.cancel();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <div className="relative flex w-fit items-center">
-      <input
-        type="text"
-        ref={ref}
-        data-state={isFocused ? "focused" : "not-focused"}
-        className="inset-2 h-full rounded-sm border-2 border-gray-300 bg-gray-50 px-2 py-2 text-sm outline-blue-400 transition-all duration-300 hover:bg-gray-200 focus:bg-white focus:outline-2 [&[data-state=focused]]:pr-8 [&[data-state=focused]]:placeholder:text-gray-500 [&[data-state=not-focused]]:placeholder:text-transparent"
-        value={localSearch}
-        onChange={handleSearch}
-        placeholder="Search backlog"
-      />
-
-      <MdSearch
-        data-state={search.length ? "searching" : "not-searching"}
-        className="absolute bottom-0 right-0 top-0 mx-2 flex h-full items-center text-lg text-gray-500 [&[data-state=searching]]:hidden"
-      />
-      <button
-        data-state={search.length ? "searching" : "not-searching"}
-        onClick={handleClearSearch}
-        className="[&[data-state=not-searching]]:hidden"
-      >
-        <BsX className="absolute bottom-0 right-0 top-0 mx-2 flex h-full items-center text-lg text-gray-500 " />
-      </button>
     </div>
   );
 };
