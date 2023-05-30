941df9f716587f11d22d9bfe33f26c0b628e66c0
sebastianfdz <fdz.seb@gmail.com>
2023-05-30 08:47:59 -0600

    Adjust import paths

--- components/backlog/header.tsx
+++ components/backlog/header.tsx
@@ -1,15 +1,11 @@
 "use client";
 import React from "react";
-import { useProject } from "@/hooks/query-hooks/useProject";
-import { Avatar } from "../avatar";
-import { Button } from "../ui/button";
 import { useFiltersContext } from "@/context/useFiltersContext";
 import { type Project } from "@prisma/client";
-import { EpicFilter } from "./filter-epic";
-import { IssueTypeFilter } from "./filter-issue-type";
-import { SearchBar } from "./filter-search-bar";
-import { AddPeopleIcon } from "../svgs";
-import { NotImplemented } from "../not-implemented";
+import { EpicFilter } from "@/components/filter-epic";
+import { IssueTypeFilter } from "@/components/filter-issue-type";
+import { SearchBar } from "@/components/filter-search-bar";
+import { Members } from "../members";
 
 const BacklogHeader: React.FC<{ project: Project }> = ({ project }) => {
   const { search, setSearch } = useFiltersContext();
@@ -23,60 +19,6 @@ const BacklogHeader: React.FC<{ project: Project }> = ({ project }) => {
         <EpicFilter />
         <IssueTypeFilter />
       </div>
-    </div>
-  );
-};
-
-const Members = () => {
-  const { members } = useProject();
-  const { assignees, setAssignees } = useFiltersContext();
-  const unassigned = {
-    id: "unassigned",
-    name: "Unassigned",
-    avatar: undefined,
-    email: "",
-  };
-
-  function handleAssigneeFilter(id: string) {
-    setAssignees((prev) => {
-      if (prev.includes(id)) return prev.filter((a) => a !== id);
-      return [...prev, id];
-    });
-  }
-  if (!members) return <div />;
-
-  return (
-    <div className="flex items-center">
-      {[...members, unassigned].map((member, index) => {
-        return (
-          <div
-            key={member.id}
-            style={{ zIndex: 10 - index }}
-            className="hover:!z-10"
-          >
-            <Button
-              onClick={() => handleAssigneeFilter(member.id)}
-              customColors
-              customPadding
-              data-state={
-                assignees.includes(member.id) ? "selected" : "not-selected"
-              }
-              className="-mx-1 flex border-spacing-2 rounded-full border-2 border-transparent bg-white p-0.5 transition-all duration-75 hover:-mt-1.5 [&[data-state=selected]]:border-inprogress"
-            >
-              <Avatar src={member.avatar} alt={`${member.name}`} />
-            </Button>
-          </div>
-        );
-      })}
-
-      <NotImplemented feature="add people">
-        <button>
-          <AddPeopleIcon
-            className="ml-3 rounded-full bg-gray-200 p-1 text-gray-500"
-            size={35}
-          />
-        </button>
-      </NotImplemented>
     </div>
   );
 };
