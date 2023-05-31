bfbbeac14cc9ca1d9a8cde1e9dfe196b3a212141
sebastianfdz <fdz.seb@gmail.com>
2023-05-30 22:02:25 -0600

    Add droppable strict mode hook

---
+++ hooks/useStrictModeDroppable.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export const useStrictModeDroppable = (): [boolean] => {
+  // This is a hack to get around the fact that react-beautiful-dnd doesn't work very well in StrictMode.
+  const [enabled, setEnabled] = useState(false);
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+  return [enabled];
+};
