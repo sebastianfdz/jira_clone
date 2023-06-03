12f2a74827e4839cd8403b7e3daafc9b8918a1dd
sebastianfdz <fdz.seb@gmail.com>
2023-06-03 08:56:01 -0600

    Add type, title, menu and avatar

--- components/board/issue.tsx
+++ components/board/issue.tsx
@@ -1,6 +1,11 @@
 import { type IssueType } from "@/utils/types";
 import clsx from "clsx";
 import { Draggable } from "react-beautiful-dnd";
+import { IssueIcon } from "../issue/issue-icon";
+import { Avatar } from "../avatar";
+import { IssueDropdownMenu } from "../issue/issue-menu";
+import { DropdownTrigger } from "../ui/dropdown-menu";
+import { BsThreeDots } from "react-icons/bs";
 
 const Issue: React.FC<{ issue: IssueType; index: number }> = ({
   issue,
@@ -17,11 +22,36 @@ const Issue: React.FC<{ issue: IssueType; index: number }> = ({
           {...draggableProps}
           {...dragHandleProps}
           className={clsx(
-            isDragging ? "bg-blue-100" : "bg-white",
-            "group flex w-full max-w-full items-center justify-between border-[0.3px] border-gray-300 px-3 py-1.5 text-sm hover:bg-gray-50 [&[data-state=selected]]:bg-blue-100"
+            isDragging && "bg-white",
+            "group my-0.5 max-w-full rounded-[3px] border-[0.3px] border-gray-300 bg-white px-2 py-3 text-sm shadow-sm shadow-gray-300 hover:bg-gray-200 "
           )}
         >
-          {issue.name}
+          <div className="flex items-center justify-between">
+            <div>{issue.name}</div>
+            <IssueDropdownMenu issue={issue}>
+              <DropdownTrigger
+                asChild
+                className="rounded-m flex items-center gap-x-2 bg-opacity-30 px-1.5 text-xs font-semibold focus:ring-2"
+              >
+                <div className="invisible rounded-sm px-1.5 py-1.5 text-gray-700 group-hover:visible group-hover:bg-gray-100 group-hover:hover:bg-gray-300 [&[data-state=open]]:visible [&[data-state=open]]:bg-gray-700 [&[data-state=open]]:text-white">
+                  <BsThreeDots className="sm:text-xl" />
+                </div>
+              </DropdownTrigger>
+            </IssueDropdownMenu>
+          </div>
+          <div className="mt-3 flex items-center justify-between">
+            <div className="flex items-center gap-x-3">
+              <IssueIcon issueType={issue.type} />
+              <span className="text-xs font-medium text-gray-600">
+                {issue.key}
+              </span>
+            </div>
+            <Avatar
+              size={20}
+              src={issue.assignee?.avatar}
+              alt={issue.assignee?.name ?? "Unassigned"}
+            />
+          </div>
         </div>
       )}
     </Draggable>
