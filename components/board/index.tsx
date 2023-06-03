8b6844a52a6d59e99769b420355cba66b6d94081
sebastianfdz <fdz.seb@gmail.com>
2023-06-03 08:30:29 -0600

    Import issue and issue list from other files

--- components/board/index.tsx
+++ components/board/index.tsx
@@ -1,21 +1,16 @@
 "use client";
-import React, { Fragment, useLayoutEffect } from "react";
+import React, { useLayoutEffect } from "react";
 import { type Sprint, type IssueStatus, type Project } from "@prisma/client";
 // import { useSelectedIssueContext } from "@/context/useSelectedIssueContext";
 import "@/styles/split.css";
 import { BoardHeader } from "./header";
-import {
-  DragDropContext,
-  Draggable,
-  type DropResult,
-  Droppable,
-} from "react-beautiful-dnd";
-import clsx from "clsx";
+import { DragDropContext, type DropResult } from "react-beautiful-dnd";
 import { useIssues } from "@/hooks/query-hooks/useIssues";
 import { useSprints } from "@/hooks/query-hooks/useSprints";
 import { type IssueType } from "@/utils/types";
-import { useStrictModeDroppable } from "@/hooks/useStrictModeDroppable";
+
 import { isNullish } from "@/utils/helpers";
+import { IssueList } from "./issue-list";
 
 const STATUSES: IssueStatus[] = ["TODO", "IN_PROGRESS", "DONE"];
 
@@ -73,89 +68,17 @@ const Board: React.FC<{
       <DragDropContext onDragEnd={onDragEnd}>
         <div className="flex min-h-full w-full max-w-full gap-x-4 overflow-y-auto">
           {STATUSES.map((status) => (
-            <div key={status} className="w-[300px]">
-              <h2 className="text-xs text-gray-400">
-                {status}{" "}
-                {issues.filter((issue) => issue.status == status).length}
-              </h2>
-              <Column
-                status={status}
-                issues={issues.filter((issue) =>
-                  filterIssuesBySprintAndStatus({ issue, status, sprints })
-                )}
-              />
-            </div>
+            <IssueList
+              key={status}
+              status={status}
+              issues={issues.filter((issue) =>
+                filterIssuesBySprintAndStatus({ issue, status, sprints })
+              )}
+            />
           ))}
         </div>
       </DragDropContext>
     </div>
-  );
-};
-
-const Column: React.FC<{ status: string; issues: IssueType[] }> = ({
-  status,
-  issues,
-}) => {
-  const [droppableEnabled] = useStrictModeDroppable();
-
-  if (!droppableEnabled) {
-    return null;
-  }
-
-  return (
-    <Droppable droppableId={status}>
-      {({ droppableProps, innerRef, placeholder }) => (
-        <div
-          {...droppableProps}
-          ref={innerRef}
-          className={clsx(issues.length == 0 && "min-h-[1px]")}
-        >
-          <Fragment>
-            {issues
-              .sort((a, b) => {
-                if (
-                  !isNullish(a.boardPosition) &&
-                  !isNullish(b.boardPosition)
-                ) {
-                  return a.boardPosition - b.boardPosition;
-                } else {
-                  return a.sprintPosition - b.sprintPosition;
-                }
-              })
-              .map((issue, index) => (
-                <Issue key={issue.key} index={index} issue={issue} />
-              ))}
-          </Fragment>
-          {placeholder}
-        </div>
-      )}
-    </Droppable>
-  );
-};
-
-const Issue: React.FC<{ issue: IssueType; index: number }> = ({
-  issue,
-  index,
-}) => {
-  return (
-    <Draggable draggableId={issue.key} index={index}>
-      {({ innerRef, dragHandleProps, draggableProps }, { isDragging }) => (
-        <div
-          role="button"
-          // data-state={issueId == issue.key ? "selected" : "not-selected"}
-          // onClick={() => setIssueId(issue.key)}
-          ref={innerRef}
-          {...draggableProps}
-          {...dragHandleProps}
-          className={clsx(
-            isDragging ? "bg-blue-100" : "bg-white",
-            "group flex w-full max-w-full items-center justify-between border-[0.3px] border-gray-300 px-3 py-1.5 text-sm hover:bg-gray-50 [&[data-state=selected]]:bg-blue-100"
-          )}
-        >
-          {issue.name}
-        </div>
-      )}
-    </Draggable>
   );
 };
 
