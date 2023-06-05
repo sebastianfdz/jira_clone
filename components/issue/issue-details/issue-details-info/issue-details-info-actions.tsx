c0b475ca5cc355f9a9658f5fccd73ed0a690c709
sebastianfdz <fdz.seb@gmail.com>
2023-06-04 18:41:03 -0600

    Add onAddChildIssue prop

--- components/issue/issue-details/issue-details-info/issue-details-info-actions.tsx
+++ components/issue/issue-details/issue-details-info/issue-details-info-actions.tsx
@@ -1,11 +1,14 @@
 import { NotImplemented } from "@/components/not-implemented";
 import { ChildrenTreeIcon } from "@/components/svgs";
 import { Button } from "@/components/ui/button";
+import { TooltipWrapper } from "@/components/ui/tooltip";
 import { BiLink } from "react-icons/bi";
 import { BsThreeDots } from "react-icons/bs";
 import { CgAttachment } from "react-icons/cg";
 
-const IssueDetailsInfoActions = () => {
+const IssueDetailsInfoActions: React.FC<{ onAddChildIssue: () => void }> = ({
+  onAddChildIssue,
+}) => {
   return (
     <div className="flex gap-x-2 text-gray-700">
       <NotImplemented feature="attachment">
@@ -13,9 +16,15 @@ const IssueDetailsInfoActions = () => {
           <CgAttachment className="rotate-45 text-xl" />
         </Button>
       </NotImplemented>
-      <Button customColors className="bg-gray-100 hover:bg-gray-200">
-        <ChildrenTreeIcon />
-      </Button>
+      <TooltipWrapper text="Add child issue">
+        <Button
+          onClick={onAddChildIssue}
+          customColors
+          className="bg-gray-100 hover:bg-gray-200"
+        >
+          <ChildrenTreeIcon />
+        </Button>
+      </TooltipWrapper>
       <NotImplemented feature="link">
         <Button customColors className="bg-gray-100 hover:bg-gray-200">
           <BiLink className="text-xl" />
