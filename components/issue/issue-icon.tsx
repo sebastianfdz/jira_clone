fffc9b1ce1a18c346e67dfd8f90adfa2f04e3c9a
sebastianfdz <fdz.seb@gmail.com>
2023-06-04 18:40:12 -0600

    Add subtask icon

--- components/issue/issue-icon.tsx
+++ components/issue/issue-icon.tsx
@@ -4,6 +4,7 @@ import { FaCheck } from "react-icons/fa";
 import { HiLightningBolt } from "react-icons/hi";
 import clsx from "clsx";
 import { type Issue as IssueType } from "@prisma/client";
+import { SubTaskIcon } from "../svgs";
 
 type IssueIconProps = {
   issueType: IssueType["type"];
@@ -17,6 +18,14 @@ const Icon: React.FC<{ children: ReactNode; className?: string }> = ({
     <div className={clsx("rounded-sm  p-0.5 text-sm text-white", className)}>
       {children}
     </div>
+  );
+};
+
+const _SubTaskIcon = () => {
+  return (
+    <Icon className="h-fit bg-task">
+      <SubTaskIcon className="text-white" />
+    </Icon>
   );
 };
 
@@ -57,6 +66,7 @@ const IssueIcon: React.FC<IssueIconProps> = ({ issueType }) => {
   if (issueType === "STORY") return <StoryIcon />;
   if (issueType === "BUG") return <BugIcon />;
   if (issueType === "EPIC") return <EpicIcon />;
+  if (issueType === "SUBTASK") return <_SubTaskIcon />;
   return null;
 };
 
