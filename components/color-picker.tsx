7bcb32554085cd51f0d5560239a7d7ad8ad873d1
sebastianfdz <fdz.seb@gmail.com>
2023-06-05 11:19:42 -0600

    Add color picker component

---
+++ components/color-picker.tsx
@@ -0,0 +1,104 @@
+import clsx from "clsx";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectPortal,
+  SelectTrigger,
+  SelectValue,
+  SelectViewport,
+} from "./ui/select";
+import { useState } from "react";
+import { type IssueType } from "@/utils/types";
+
+const LIGHT_COLORS = [
+  { hex: "#9f8fef", label: "purple" },
+  { hex: "#579dff", label: "blue" },
+  { hex: "#4cce97", label: "green" },
+  { hex: "#61c6d2", label: "teal" },
+  { hex: "#e1b205", label: "yellow" },
+  { hex: "#f97463", label: "red" },
+  { hex: "#8590a2", label: "gray" },
+];
+
+const DARK_COLORS = [
+  { hex: "#6e5dc6", label: "purple" },
+  { hex: "#0b66e4", label: "blue" },
+  { hex: "#20845a", label: "green" },
+  { hex: "#1e7f8c", label: "teal" },
+  { hex: "#b65c01", label: "yellow" },
+  { hex: "#ca3521", label: "red" },
+  { hex: "#616f86", label: "gray" },
+];
+
+const ColorPicker: React.FC<{ issue: IssueType }> = ({ issue }) => {
+  const [selected, setSelected] = useState<string | null>(
+    issue.sprintColor ?? null
+  );
+  function handleSelectChange(value: string) {
+    setSelected(value);
+  }
+
+  return (
+    <Select onValueChange={handleSelectChange}>
+      <SelectTrigger
+        onClick={(e) => e.stopPropagation()}
+        // disabled={isUpdating}
+        className={clsx("flex w-fit items-center gap-x-1 whitespace-nowrap")}
+      >
+        <SelectValue asChild>
+          <ColorSquare color={selected ?? "#9f8fef"} />
+        </SelectValue>
+      </SelectTrigger>
+      <SelectPortal className="z-[999] w-full">
+        <SelectContent position="popper">
+          <SelectViewport className="flex w-full flex-col gap-y-2 rounded-md border border-gray-300 bg-white p-2 shadow-md">
+            <SelectGroup>
+              <div className="m-0 flex items-center gap-x-2 p-0">
+                {LIGHT_COLORS.map((color) => (
+                  <SelectItem
+                    asChild
+                    key={color.hex}
+                    value={color.hex}
+                    data-state={color.hex == selected ? "checked" : "unchecked"}
+                    noBorder
+                  >
+                    <ColorSquare color={color.hex} />
+                  </SelectItem>
+                ))}
+              </div>
+            </SelectGroup>
+            <SelectGroup>
+              <div className="m-0 flex items-center gap-x-2 p-0">
+                {DARK_COLORS.map((color) => (
+                  <SelectItem
+                    asChild
+                    key={color.hex}
+                    value={color.hex}
+                    data-state={color.hex == selected ? "checked" : "unchecked"}
+                    noBorder
+                  >
+                    <ColorSquare color={color.hex} />
+                  </SelectItem>
+                ))}
+              </div>
+            </SelectGroup>
+          </SelectViewport>
+        </SelectContent>
+      </SelectPortal>
+    </Select>
+  );
+};
+
+const ColorSquare: React.FC<{ color: string }> = ({ color }) => {
+  return (
+    <div
+      role="button"
+      className="h-6 w-6 rounded-[3px] p-0"
+      style={{ backgroundColor: color }}
+    ></div>
+  );
+};
+
+export { ColorPicker };
