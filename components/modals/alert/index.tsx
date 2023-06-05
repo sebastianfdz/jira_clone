f2f9ba640129c58e2d0c63efdfdbd2f7dde5212b
sebastianfdz <fdz.seb@gmail.com>
2023-06-05 10:04:12 -0600

    Make modal controlled with state and add bold description

--- components/modals/alert/index.tsx
+++ components/modals/alert/index.tsx
@@ -1,49 +1,75 @@
 import {
   Modal,
-  ModalAction,
   ModalCancel,
   ModalContent,
   ModalDescription,
   ModalOverlay,
   ModalPortal,
   ModalTitle,
-  ModalTrigger,
 } from "@/components/ui/alert-modal";
 import { Button } from "@/components/ui/button";
-import { type ReactNode } from "react";
+import { IoWarning } from "react-icons/io5";
 
 const AlertModal: React.FC<{
-  children: ReactNode;
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   title: string;
   description: string;
   actionText: string;
   onAction: () => void;
-}> = ({ children, title, description, actionText, onAction }) => {
+}> = ({ title, description, actionText, onAction, isOpen, setIsOpen }) => {
   return (
-    <Modal>
-      <ModalTrigger asChild>{children}</ModalTrigger>
+    <Modal open={isOpen} onOpenChange={setIsOpen}>
       <ModalPortal>
         <ModalOverlay />
         <ModalContent>
-          <ModalTitle>{title}</ModalTitle>
-          <ModalDescription>{description}</ModalDescription>
-          <div className="flex items-center justify-end gap-x-3">
-            <ModalAction>
+          <div className="flex items-center gap-x-2">
+            <IoWarning className="text-xl text-red-600" />
+            <ModalTitle>{title}</ModalTitle>
+          </div>
+          <ModalDescription>
+            <BoldDescription description={description} />
+          </ModalDescription>
+          <div className="mt-5 flex items-center justify-end gap-x-3">
+            <Button
+              customColors
+              className="bg-red-600 text-white hover:bg-red-700"
+              onClick={onAction}
+            >
+              {actionText}
+            </Button>
+            <ModalCancel>
               <Button
                 customColors
-                className="bg-red-600 text-white hover:bg-red-700"
-                onClick={onAction}
+                className="transition-all duration-150 hover:bg-gray-200"
               >
-                {actionText}
+                Cancel
               </Button>
-            </ModalAction>
-            <ModalCancel>
-              <Button>Cancel</Button>
             </ModalCancel>
           </div>
         </ModalContent>
       </ModalPortal>
     </Modal>
+  );
+};
+
+const BoldDescription: React.FC<{ description: string }> = ({
+  description,
+}) => {
+  const parts = description.split("BOLD");
+
+  return (
+    <span className="text-gray-700">
+      {parts.map((part, index) => {
+        if (index % 2 === 1) {
+          // This part should be bold
+          return <strong key={index}>{part}</strong>;
+        } else {
+          // This part should not be bold
+          return part;
+        }
+      })}
+    </span>
   );
 };
 
