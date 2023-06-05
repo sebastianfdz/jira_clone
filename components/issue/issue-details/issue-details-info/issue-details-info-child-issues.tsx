ea3cd462a427ca09f6ca50e80bfee126940cc11a
sebastianfdz <fdz.seb@gmail.com>
2023-06-04 18:40:27 -0600

    Add subtask creation

--- components/issue/issue-details/issue-details-info/issue-details-info-child-issues.tsx
+++ components/issue/issue-details/issue-details-info/issue-details-info-child-issues.tsx
@@ -1,11 +1,8 @@
-import { useIssues } from "@/hooks/query-hooks/useIssues";
 import { IssueIcon } from "../../issue-icon";
 import { IssueTitle } from "../../issue-title";
 import { Button } from "@/components/ui/button";
 import { IssueContextMenu } from "../../issue-menu";
 import { MdEdit } from "react-icons/md";
-import { NotImplemented } from "@/components/not-implemented";
-import { ChildrenTreeIcon } from "@/components/svgs";
 import { IssueSelectStatus } from "../../issue-select-status";
 import { IssueAssigneeSelect } from "../../issue-select-assignee";
 import clsx from "clsx";
@@ -15,18 +12,83 @@ import { ContextTrigger } from "@/components/ui/context-menu";
 import { type IssueType } from "@/utils/types";
 import { getIssueCountByStatus } from "@/utils/helpers";
 import { TooltipWrapper } from "@/components/ui/tooltip";
-import { Issue } from "@prisma/client";
+
+import { AiOutlinePlus } from "react-icons/ai";
+import { EmtpyIssue } from "@/components/backlog/issue-empty";
+import { useIssues } from "@/hooks/query-hooks/useIssues";
 
 const ChildIssueList: React.FC<{
   issues: IssueType[];
-}> = ({ issues }) => {
+  parentIsEpic: boolean;
+  parentKey: IssueType["key"];
+  isAddingChildIssue: boolean;
+  setIsAddingChildIssue: React.Dispatch<React.SetStateAction<boolean>>;
+}> = ({
+  issues,
+  parentIsEpic,
+  parentKey,
+  isAddingChildIssue,
+  setIsAddingChildIssue,
+}) => {
+  const { createIssue, isCreating } = useIssues();
+  const [isEditing, setIsEditing] = useState(isAddingChildIssue);
+
+  function handleCreateIssue({
+    name,
+    type,
+    parentKey,
+  }: {
+    name: string;
+    type: IssueType["type"];
+    parentKey: IssueType["key"] | null;
+  }) {
+    createIssue(
+      {
+        name,
+        type,
+        parentKey,
+        sprintId: null,
+        reporterId: null,
+      },
+      {
+        onSuccess: () => {
+          setIsEditing(false);
+          setIsAddingChildIssue(false);
+        },
+      }
+    );
+  }
   return (
     <Fragment>
-      <h2>Child Issues</h2>
-      <ProgressBar issues={issues} />
+      <div className="flex items-center justify-between">
+        <h2>Child Issues</h2>
+        <Button
+          onClick={() => setIsEditing(true)}
+          customColors
+          customPadding
+          className="p-1 hover:bg-gray-100"
+        >
+          <AiOutlinePlus />
+        </Button>
+      </div>
+      {issues.length ? <ProgressBar issues={issues} /> : null}
       {issues.map((issue) => {
         return <ChildIssue key={issue.key} issue={issue} />;
       })}
+      <EmtpyIssue
+        data-state={isEditing || isAddingChildIssue ? "open" : "closed"}
+        className="[&[data-state=closed]]:hidden"
+        onCreate={({ name, type, parentKey }) =>
+          handleCreateIssue({ name, type, parentKey })
+        }
+        onCancel={() => {
+          setIsEditing(false);
+          setIsAddingChildIssue(false);
+        }}
+        isCreating={isCreating}
+        isSubtask={!parentIsEpic}
+        parentKey={parentKey}
+      />
     </Fragment>
   );
 };
@@ -85,11 +147,6 @@ const ChildIssue: React.FC<{ issue: IssueType }> = ({ issue }) => {
         <ContextTrigger className="h-8 w-full" />
       </IssueContextMenu>
       <div className="relative ml-2 flex min-w-fit items-center justify-end gap-x-2">
-        <NotImplemented feature="child issues">
-          <button>
-            <ChildrenTreeIcon className="text-gray-600" />
-          </button>
-        </NotImplemented>
         <IssueAssigneeSelect issue={issue} avatarSize={20} avatarOnly />
         <IssueSelectStatus
           key={issue.key + issue.status}
@@ -116,35 +173,41 @@ const ProgressBar: React.FC<{ issues: IssueType[] }> = ({ issues }) => {
         style={{ width: "100%" }}
         className="flex h-2.5 gap-x-0.5 overflow-hidden rounded-full bg-white"
       >
-        <TooltipWrapper
-          text={`Done: ${statusCount.DONE} of ${issues.length} issues`}
-        >
-          <div
-            style={{ width: `${(statusCount.DONE / issues.length) * 100}%` }}
-            className="float-left h-full bg-done"
-          />
-        </TooltipWrapper>
-        <TooltipWrapper
-          text={`In Progress: ${statusCount.IN_PROGRESS} of ${issues.length} issues`}
-        >
-          <div
-            style={{
-              width: `${(statusCount.IN_PROGRESS / issues.length) * 100}%`,
-            }}
-            className="float-left h-full bg-inprogress"
-          />
-        </TooltipWrapper>
-        <TooltipWrapper
-          text={`To Do: ${statusCount.TODO} of ${issues.length} issues`}
-        >
-          <div
-            style={{ width: `${(statusCount.TODO / issues.length) * 100}%` }}
-            className="float-left h-full bg-todo"
-          />
-        </TooltipWrapper>
+        {statusCount.DONE ? (
+          <TooltipWrapper
+            text={`Done: ${statusCount.DONE} of ${issues.length} issues`}
+          >
+            <div
+              style={{ width: `${(statusCount.DONE / issues.length) * 100}%` }}
+              className="float-left h-full bg-done"
+            />
+          </TooltipWrapper>
+        ) : null}
+        {statusCount.IN_PROGRESS ? (
+          <TooltipWrapper
+            text={`In Progress: ${statusCount.IN_PROGRESS} of ${issues.length} issues`}
+          >
+            <div
+              style={{
+                width: `${(statusCount.IN_PROGRESS / issues.length) * 100}%`,
+              }}
+              className="float-left h-full bg-inprogress"
+            />
+          </TooltipWrapper>
+        ) : null}
+        {statusCount.TODO ? (
+          <TooltipWrapper
+            text={`To Do: ${statusCount.TODO} of ${issues.length} issues`}
+          >
+            <div
+              style={{ width: `${(statusCount.TODO / issues.length) * 100}%` }}
+              className="float-left h-full bg-todo"
+            />
+          </TooltipWrapper>
+        ) : null}
       </div>
       <div className=" whitespace-nowrap text-sm text-gray-500">
-        {(statusCount.DONE / issues.length) * 100}% Done
+        {((statusCount.DONE / issues.length) * 100).toFixed(0)}% Done
       </div>
     </div>
   );
