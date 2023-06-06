9044eed2f789df0890b8dd9fc186128e6c60e14a
sebastianfdz <fdz.seb@gmail.com>
2023-06-06 10:45:10 -0600

    Get metadata from config file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,39 @@
 import { ClerkProvider } from "@clerk/nextjs/app-beta";
 import { type Metadata } from "next";
+import { siteConfig } from "@/config/site";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: "Jira Clone 🚀",
-  description: "A clone of Jira built with Next.js",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Jira",
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+    "Clerk",
+    "TanStack",
+  ],
+  authors: [
+    {
+      name: "Sebastian Fernandez",
+      url: "https://sebastianfdz.com",
+    },
+  ],
+  creator: "Sebastian Fernandez",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
