e5fa02c461fe574c1dfd8fd4cd52b4b41208fc42
sebastianfdz <fdz.seb@gmail.com>
2023-06-06 10:38:32 -0600

    Add config files

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "Jira Clone 🚀",
+  description:
+    "An open source Jira clone built with Next.js 13, featuring React server components. Powered by tailwind, prisma, tanstack query, radix ui, lexical, axios, and zod. Efficient project management at your fingertips.",
+  url: "https://jira.sebastianfdz.com",
+  links: {
+    github: "https://github.com/sebastianfdz/jira_clone",
+  },
+};
