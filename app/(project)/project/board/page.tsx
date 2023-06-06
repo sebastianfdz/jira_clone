42e3c5926936f6cf81f970d20a98218a1fcf4907
sebastianfdz <fdz.seb@gmail.com>
2023-06-06 11:31:31 -0600

    Remove imported server functions and declare them directly in file

--- app/(project)/project/board/page.tsx
+++ app/(project)/project/board/page.tsx
@@ -1,18 +1,63 @@
 import { Board } from "@/components/board";
 import { notFound } from "next/navigation";
-import { getProjectFromServer } from "@/app/api/project/route";
-import { getIssuesFromServer } from "@/app/api/issues/route";
-import { getSprintsFromServer } from "@/app/api/sprints/route";
 import { type Metadata } from "next";
+import { prisma } from "@/server/db";
+import { clerkClient } from "@clerk/nextjs";
+import { filterUserForClient, generateIssuesForClient } from "@/utils/helpers";
+import { SprintStatus } from "@prisma/client";
 
 export const metadata: Metadata = {
   title: "Board",
 };
 
+async function getIssuesFromServer() {
+  const activeIssues = await prisma.issue.findMany({
+    where: { isDeleted: false },
+  });
+  if (!activeIssues) {
+    return [];
+  }
+
+  const userIds = activeIssues
+    .flatMap((issue) => [issue.assigneeId, issue.reporterId] as string[])
+    .filter(Boolean);
+
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: userIds,
+      limit: 20,
+    })
+  ).map(filterUserForClient);
+
+  const issues = generateIssuesForClient(activeIssues, users);
+  return issues;
+}
+
+async function getProjectFromServer() {
+  const project = await prisma.project.findUnique({
+    where: { key: "JIRA-CLONE" },
+  });
+  return project;
+}
+
+async function getSprintsFromServer() {
+  const sprints = await prisma.sprint.findMany({
+    where: {
+      OR: [{ status: SprintStatus.ACTIVE }, { status: SprintStatus.PENDING }],
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+  return sprints;
+}
+
 const BoardPage = async () => {
-  const project = await getProjectFromServer();
-  const issues = await getIssuesFromServer();
-  const sprints = await getSprintsFromServer();
+  const [project, issues, sprints] = await Promise.all([
+    getProjectFromServer(),
+    getIssuesFromServer(),
+    getSprintsFromServer(),
+  ]);
 
   if (!project || !issues || !sprints) {
     return notFound();
