e874b30db8313a96a6b73ba9700899238a582dbf
sebastianfdz <fdz.seb@gmail.com>
2023-06-06 10:45:48 -0600

    Remove use client directive

---
+++ app/(project)/project/backlog/not-found.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@/components/ui/container";
+
+const BacklogErrorPage = () => {
+  return <Container screen>Project Not Found</Container>;
+};
+
+export default BacklogErrorPage;
---
+++ app/(project)/project/board/not-found.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@/components/ui/container";
+
+const BoardErrorPage = () => {
+  return <Container screen>Project Not Found</Container>;
+};
+
+export default BoardErrorPage;
