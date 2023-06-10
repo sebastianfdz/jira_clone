8943c6664dd8edad05a962dd89c501c6d9fcb3a3
sebastianfdz <fdz.seb@gmail.com>
2023-06-10 09:56:38 -0600

    Cache query client

---
+++ utils/provider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  QueryClientProvider,
+  //  QueryClient
+} from "@tanstack/react-query";
+import { getQueryClient } from "./get-query-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function Providers({ children }: React.PropsWithChildren) {
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") setIsBrowser(true);
+  }, []);
+
+  const [client] = useState(
+    () => getQueryClient()
+    // new QueryClient()
+    // { defaultOptions: { queries: { staleTime: 1000 } } }
+  );
+
+  if (!isBrowser) return null;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
