bc7dc73b45bcd9202a0a25fd7474f49bf12e8217
sebastianfdz <fdz.seb@gmail.com>
2023-06-10 10:04:08 -0600

    Move query client wrapper to root layout

--- app/(project)/project/layout.tsx
+++ app/(project)/project/layout.tsx
@@ -1,20 +1,11 @@
 import { Sidebar } from "@/components/sidebar";
 import { TopNavbar } from "@/components/top-navbar";
 import { FiltersProvider } from "@/context/useFiltersContext";
-import Toaster from "@/components/toast";
-import { ClientProvider } from "@/app/(project)/project/query-client";
+import { Fragment } from "react";
 
 const ProjectLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClientProvider>
-      <Toaster
-        position="bottom-left"
-        reverseOrder={false}
-        containerStyle={{
-          height: "92vh",
-          marginLeft: "3vw",
-        }}
-      />
+    <Fragment>
       <TopNavbar />
       <main className="flex h-[calc(100vh_-_3rem)] w-full">
         <Sidebar />
@@ -22,7 +13,7 @@ const ProjectLayout = ({ children }: { children: React.ReactNode }) => {
           <div className="w-full max-w-[calc(100vw_-_16rem)]">{children}</div>
         </FiltersProvider>
       </main>
-    </ClientProvider>
+    </Fragment>
   );
 };
 
