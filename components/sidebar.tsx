6310fe5816cd3a1315fe2f7269b284ed6dfef7c1
sebastianfdz <fdz.seb@gmail.com>
2023-06-16 07:12:28 -0600

    Disable development sidebar elements

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { BacklogIcon, BoardIcon, DevelopmentIcon, RoadmapIcon } from "./svgs";
 import {
@@ -11,62 +11,14 @@ import { usePathname } from "next/navigation";
 import { FaChessPawn, FaChevronRight } from "react-icons/fa";
 import { useProject } from "@/hooks/query-hooks/use-project";
 
-type NavItem = {
+type NavItemType = {
   id: string;
   label: string;
   icon: React.FC<{ className?: string }>;
   href: string;
 };
 
-const NavList: React.FC<{ items: NavItem[]; label: string }> = ({
-  items,
-  label,
-}) => {
-  const [isVisible, setIsVisible] = useState(true);
-  const currentPath = usePathname();
-  return (
-    <div className="flex flex-col gap-y-2">
-      <div className="group flex items-center gap-x-1">
-        <button
-          data-state={isVisible ? "open" : "closed"}
-          onClick={() => setIsVisible(!isVisible)}
-          className="invisible group-hover:visible [&[data-state=open]>svg]:rotate-90"
-        >
-          <FaChevronRight className="text-xs transition-transform" />
-        </button>
-        <span className="text-xs font-bold text-gray-700">{label}</span>
-      </div>
-      <NavigationMenu
-        data-state={isVisible ? "open" : "closed"}
-        className="hidden [&[data-state=open]]:block"
-      >
-        <NavigationMenuList>
-          {items.map((item) => (
-            <Link
-              key={item.id}
-              href={item.href}
-              className="w-full rounded-lg text-gray-600"
-              passHref
-              legacyBehavior
-            >
-              <NavigationMenuLink
-                active={currentPath === item.href}
-                className="flex w-full rounded-sm border-transparent py-2 [&[data-active]]:border-l-blue-700 [&[data-active]]:bg-blue-100 [&[data-active]]:text-blue-700"
-              >
-                <div className="flex w-full items-center gap-x-3 border-l-4 border-inherit bg-inherit px-2">
-                  <item.icon />
-                  <span className="text-sm">{item.label}</span>
-                </div>
-              </NavigationMenuLink>
-            </Link>
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
-  );
-};
-
-const Sidebar = () => {
+const Sidebar: React.FC = () => {
   const { project } = useProject();
   const planningItems = [
     {
@@ -113,6 +65,88 @@ const Sidebar = () => {
       <NavList label={"PLANNING"} items={planningItems} />
       <NavList label={"DEVELOPMENT"} items={developmentItems} />
     </div>
+  );
+};
+
+const NavList: React.FC<{ items: NavItemType[]; label: string }> = ({
+  items,
+  label,
+}) => {
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <div className="flex flex-col gap-y-2">
+      <NavListHeader
+        label={label}
+        isVisible={isVisible}
+        setIsVisible={setIsVisible}
+      />
+      <NavigationMenu
+        data-state={isVisible ? "open" : "closed"}
+        className="hidden [&[data-state=open]]:block"
+      >
+        <NavigationMenuList>
+          {items.map((item) => (
+            <NavItem
+              key={item.id}
+              item={item}
+              disabled={label === "DEVELOPMENT"}
+            />
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+};
+
+const NavListHeader: React.FC<{
+  label: string;
+  isVisible: boolean;
+  setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}> = ({ label, isVisible, setIsVisible }) => (
+  <div className="group flex items-center gap-x-1">
+    <button
+      data-state={isVisible ? "open" : "closed"}
+      onClick={() => setIsVisible(!isVisible)}
+      className="invisible group-hover:visible [&[data-state=open]>svg]:rotate-90"
+    >
+      <FaChevronRight className="text-xs transition-transform" />
+    </button>
+    <span className="text-xs font-bold text-gray-700">{label}</span>
+  </div>
+);
+
+const NavItem: React.FC<{ item: NavItemType; disabled?: boolean }> = ({
+  item,
+  disabled = false,
+}) => {
+  const currentPath = usePathname();
+  if (disabled) {
+    return (
+      <div className="w-full rounded-lg text-gray-600 hover:cursor-not-allowed">
+        <div className="flex w-full items-center gap-x-3 border-l-4 border-transparent px-2 py-2">
+          <item.icon />
+          <span className="text-sm">{item.label}</span>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <Link
+      href={item.href}
+      className="w-full rounded-lg text-gray-600 "
+      passHref
+      legacyBehavior
+    >
+      <NavigationMenuLink
+        active={currentPath === item.href}
+        className="flex w-full rounded-sm border-transparent py-2 [&[data-active]]:border-l-blue-700 [&[data-active]]:bg-blue-100 [&[data-active]]:text-blue-700"
+      >
+        <div className="flex w-full items-center gap-x-3 border-l-4 border-inherit bg-inherit px-2">
+          <item.icon />
+          <span className="text-sm">{item.label}</span>
+        </div>
+      </NavigationMenuLink>
+    </Link>
   );
 };
 
