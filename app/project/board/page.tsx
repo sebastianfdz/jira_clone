63ad6fd24cda56bd1e2dcbb61e7885fad2a90974
sebastianfdz <fdz.seb@gmail.com>
2023-06-19 17:09:27 -0600

    Prefetch from server from react query

--- app/project/board/page.tsx
+++ app/project/board/page.tsx
@@ -3,19 +3,29 @@ import { type Metadata } from "next";
 import { getQueryClient } from "@/utils/get-query-client";
 import { Hydrate } from "@/utils/hydrate";
 import { dehydrate } from "@tanstack/query-core";
-import { api } from "@/utils/api";
+import { currentUser } from "@clerk/nextjs";
+import {
+  getIssuesFromServer,
+  getProjectFromServer,
+  getSprintsFromServer,
+} from "@/server/db";
 
 export const metadata: Metadata = {
   title: "Board",
 };
 
 const BoardPage = async () => {
+  const user = await currentUser();
   const queryClient = getQueryClient();
 
   await Promise.all([
-    await queryClient.prefetchQuery(["issues"], api.issues.getIssues),
-    await queryClient.prefetchQuery(["sprints"], api.sprints.getSprints),
-    await queryClient.prefetchQuery(["project"], api.project.getProject),
+    await queryClient.prefetchQuery(["issues"], () =>
+      getIssuesFromServer(user?.id)
+    ),
+    await queryClient.prefetchQuery(["sprints"], () =>
+      getSprintsFromServer(user?.id)
+    ),
+    await queryClient.prefetchQuery(["project"], getProjectFromServer),
   ]);
 
   const dehydratedState = dehydrate(queryClient);
--- app/project/roadmap/page.tsx
+++ app/project/roadmap/page.tsx
@@ -3,19 +3,29 @@ import { getQueryClient } from "@/utils/get-query-client";
 import { Hydrate } from "@/utils/hydrate";
 import { dehydrate } from "@tanstack/query-core";
 import { Roadmap } from "@/components/roadmap";
-import { api } from "@/utils/api";
+import {
+  getIssuesFromServer,
+  getProjectFromServer,
+  getSprintsFromServer,
+} from "@/server/db";
+import { currentUser } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Roadmap",
 };
 
 const RoadmapPage = async () => {
+  const user = await currentUser();
   const queryClient = getQueryClient();
 
   await Promise.all([
-    await queryClient.prefetchQuery(["issues"], api.issues.getIssues),
-    await queryClient.prefetchQuery(["sprints"], api.sprints.getSprints),
-    await queryClient.prefetchQuery(["project"], api.project.getProject),
+    await queryClient.prefetchQuery(["issues"], () =>
+      getIssuesFromServer(user?.id)
+    ),
+    await queryClient.prefetchQuery(["sprints"], () =>
+      getSprintsFromServer(user?.id)
+    ),
+    await queryClient.prefetchQuery(["project"], getProjectFromServer),
   ]);
 
   const dehydratedState = dehydrate(queryClient);
