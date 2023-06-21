571eb9e66cb6ffd7ead6290ed8af977af2d99b65
sebastianfdz <fdz.seb@gmail.com>
2023-06-20 21:48:06 -0600

    Update sprint description model

--- app/api/sprints/route.ts
+++ app/api/sprints/route.ts
@@ -28,6 +28,7 @@ export async function POST(req: NextRequest) {
   const sprint = await prisma.sprint.create({
     data: {
       name: `SPRINT-${k}`,
+      description: "",
       creatorId: userId,
     },
   });
