c1af1b3fff90c2914c4420e08b2859794b82f5bd
sebastianfdz <fdz.seb@gmail.com>
2023-07-05 10:02:30 -0600

    Add unassigned user icon

--- components/svgs.tsx
+++ components/svgs.tsx
@@ -452,6 +452,27 @@ const SubTaskIcon: React.FC<{ className?: string; size?: number }> = ({
   );
 };
 
+const UnassignedUser: React.FC<{ className?: string; size?: number }> = ({
+  className,
+  size,
+}) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width={size ?? 14}
+      height={size ?? 14}
+      viewBox="0 0 24 24"
+      role="presentation"
+    >
+      <g fill="currentColor" fillRule="evenodd">
+        <path d="M6 14c0-1.105.902-2 2.009-2h7.982c1.11 0 2.009.894 2.009 2.006v4.44c0 3.405-12 3.405-12 0V14z"></path>
+        <circle cx="12" cy="7" r="4"></circle>
+      </g>
+    </svg>
+  );
+};
+
 export {
   ChildrenTreeIcon,
   BacklogIcon,
@@ -462,4 +483,5 @@ export {
   SprintTrophy,
   AddPeopleIcon,
   SubTaskIcon,
+  UnassignedUser,
 };
